fc62197166059ea5ba94d51acd1ff547810b539f
Lukasz Osipiuk <lukasz@osipiuk.net>
2016-02-03 15:51:58 +0100

    Treat fixed pointer literals as DECIMAL

--- presto-parser/src/main/antlr4/com/facebook/presto/sql/parser/SqlBase.g4
+++ presto-parser/src/main/antlr4/com/facebook/presto/sql/parser/SqlBase.g4
@@ -429,6 +429,7 @@ quotedIdentifier
 
 number
     : DECIMAL_VALUE  #decimalLiteral
+    | DOUBLE_VALUE   #doubleLiteral
     | INTEGER_VALUE  #integerLiteral
     ;
 
@@ -672,7 +673,10 @@ INTEGER_VALUE
 DECIMAL_VALUE
     : DIGIT+ '.' DIGIT*
     | '.' DIGIT+
-    | DIGIT+ ('.' DIGIT*)? EXPONENT
+    ;
+
+DOUBLE_VALUE
+    : DIGIT+ ('.' DIGIT*)? EXPONENT
     | '.' DIGIT+ EXPONENT
     ;
 
--- presto-parser/src/main/java/com/facebook/presto/sql/parser/AstBuilder.java
+++ presto-parser/src/main/java/com/facebook/presto/sql/parser/AstBuilder.java
@@ -1362,7 +1362,7 @@ class AstBuilder
 
         if (context.DOUBLE_PRECISION() != null) {
             // TODO: Temporary hack that should be removed with new planner.
-            return new GenericLiteral(getLocation(context), "DOUBLE", value);
+            return new DoubleLiteral(getLocation(context), value);
         }
 
         String type = context.identifier().getText();
@@ -1378,6 +1378,9 @@ class AstBuilder
         if (type.equalsIgnoreCase("char")) {
             return new CharLiteral(getLocation(context), value);
         }
+        if (type.equalsIgnoreCase("double")) {
+            return new DoubleLiteral(getLocation(context), value);
+        }
 
         return new GenericLiteral(getLocation(context), type, value);
     }
@@ -1390,6 +1393,12 @@ class AstBuilder
 
     @Override
     public Node visitDecimalLiteral(SqlBaseParser.DecimalLiteralContext context)
+    {
+        return new DecimalLiteral(getLocation(context), context.getText());
+    }
+
+    @Override
+    public Node visitDoubleLiteral(SqlBaseParser.DoubleLiteralContext context)
     {
         return new DoubleLiteral(getLocation(context), context.getText());
     }
