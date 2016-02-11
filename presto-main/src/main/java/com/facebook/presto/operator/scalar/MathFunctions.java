14208adcd46839bde07c46b5960d5405dde87bb8
Wojciech Biela <Wojciech.Biela@teradata.com>
2016-02-11 17:03:20 +0100

    "mod" scalar function for decimal(p,s)
    This implementation reuses the implementation from the mod operator for
    decimal(p,s)
    
    Conflicts:
      presto-main/src/main/java/com/facebook/presto/type/DecimalOperators.java
    
    Conflicts:
    	presto-main/src/test/java/com/facebook/presto/operator/scalar/TestMathFunctions.java
    
    Conflicts:
    	presto-main/src/main/java/com/facebook/presto/type/DecimalOperators.java
    	presto-main/src/test/java/com/facebook/presto/operator/scalar/TestMathFunctions.java

--- presto-main/src/main/java/com/facebook/presto/operator/scalar/MathFunctions.java
+++ presto-main/src/main/java/com/facebook/presto/operator/scalar/MathFunctions.java
@@ -48,6 +48,8 @@ import static com.facebook.presto.spi.type.DoubleType.DOUBLE;
 import static com.facebook.presto.spi.type.StandardTypes.BIGINT;
 import static com.facebook.presto.spi.type.TypeSignature.parseTypeSignature;
 import static com.facebook.presto.spi.type.VarcharType.VARCHAR;
+import static com.facebook.presto.type.DecimalOperators.modulusScalarFunction;
+import static com.facebook.presto.type.DecimalOperators.modulusSignatureBuilder;
 import static com.facebook.presto.util.Failures.checkCondition;
 import static io.airlift.slice.Slices.utf8Slice;
 import static java.lang.Character.MAX_RADIX;
@@ -62,6 +64,7 @@ public final class MathFunctions
 {
     public static final SqlScalarFunction[] DECIMAL_CEILING_FUNCTIONS = {decimalCeilingFunction("ceiling"), decimalCeilingFunction("ceil")};
     public static final SqlScalarFunction DECIMAL_FLOOR_FUNCTION = decimalFloorFunction();
+    public static final SqlScalarFunction DECIMAL_MOD_FUNCTION = decimalModFunction();
 
     private MathFunctions() {}
 
@@ -497,6 +500,15 @@ public final class MathFunctions
     public static double mod(@SqlType(StandardTypes.DOUBLE) double num1, @SqlType(StandardTypes.DOUBLE) double num2)
     {
         return num1 % num2;
+    }
+
+    private static SqlScalarFunction decimalModFunction()
+    {
+        Signature signature = modulusSignatureBuilder()
+                .kind(SCALAR)
+                .name("mod")
+                .build();
+        return modulusScalarFunction(signature);
     }
 
     @Description("remainder of given quotient")
