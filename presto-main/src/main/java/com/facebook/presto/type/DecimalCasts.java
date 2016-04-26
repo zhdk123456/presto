fcec72bb7f78ec0d69711d9ad159671cb776b259
Andrzej Fiedukowicz <Andrzej.Fiedukowicz@teradata.com>
2016-04-26 10:45:22 +0200

    Add support of varchar(x) to presto scalar functions and operators

--- presto-main/src/main/java/com/facebook/presto/type/DecimalCasts.java
+++ presto-main/src/main/java/com/facebook/presto/type/DecimalCasts.java
@@ -59,7 +59,6 @@ import static com.facebook.presto.spi.type.StandardTypes.JSON;
 import static com.facebook.presto.spi.type.StandardTypes.REAL;
 import static com.facebook.presto.spi.type.StandardTypes.SMALLINT;
 import static com.facebook.presto.spi.type.StandardTypes.TINYINT;
-import static com.facebook.presto.spi.type.StandardTypes.VARCHAR;
 import static com.facebook.presto.spi.type.TypeSignature.parseTypeSignature;
 import static com.facebook.presto.util.Failures.checkCondition;
 import static com.facebook.presto.util.Types.checkType;
@@ -87,8 +86,8 @@ public final class DecimalCasts
     public static final SqlScalarFunction DOUBLE_TO_DECIMAL_CAST = castFunctionToDecimalFrom(DOUBLE, "doubleToShortDecimal", "doubleToLongDecimal");
     public static final SqlScalarFunction DECIMAL_TO_REAL_CAST = castFunctionFromDecimalTo(REAL, "shortDecimalToReal", "longDecimalToReal");
     public static final SqlScalarFunction REAL_TO_DECIMAL_CAST = castFunctionToDecimalFrom(REAL, "realToShortDecimal", "realToLongDecimal");
-    public static final SqlScalarFunction DECIMAL_TO_VARCHAR_CAST = castFunctionFromDecimalTo(VARCHAR, "shortDecimalToVarchar", "longDecimalToVarchar");
-    public static final SqlScalarFunction VARCHAR_TO_DECIMAL_CAST = castFunctionToDecimalFrom(VARCHAR, "varcharToShortDecimal", "varcharToLongDecimal");
+    public static final SqlScalarFunction DECIMAL_TO_VARCHAR_CAST = castFunctionFromDecimalTo("varchar(x)", "shortDecimalToVarchar", "longDecimalToVarchar");
+    public static final SqlScalarFunction VARCHAR_TO_DECIMAL_CAST = castFunctionToDecimalFrom("varchar(x)", "varcharToShortDecimal", "varcharToLongDecimal");
     public static final SqlScalarFunction DECIMAL_TO_JSON_CAST = castFunctionFromDecimalTo(JSON, "shortDecimalToJson", "longDecimalToJson");
     public static final SqlScalarFunction JSON_TO_DECIMAL_CAST = castFunctionToDecimalFromBuilder(JSON, "jsonToShortDecimal", "jsonToLongDecimal").nullableResult(true).build();
 
@@ -131,7 +130,7 @@ public final class DecimalCasts
                 .kind(SCALAR)
                 .operatorType(CAST)
                 .typeVariableConstraints(withVariadicBound("X", DECIMAL))
-                .argumentTypes(parseTypeSignature(from))
+                .argumentTypes(parseTypeSignature(from, ImmutableSet.of("x", "precision", "scale")))
                 .returnType(parseTypeSignature("X"))
                 .build();
         return SqlScalarFunction.builder(DecimalCasts.class)
