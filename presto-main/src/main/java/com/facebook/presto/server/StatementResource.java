145002a3bf73e14ae72fd34fcbd82ed96d4b8eec
Andrzej Fiedukowicz <Andrzej.Fiedukowicz@teradata.com>
2016-08-02 13:07:56 +0200

    Send serialized PrestoException as response
    This change allows server to send serialized version of PrestoException
    (message, serialized stack trace, error code) in order to allow clients
    to handle error in better way.
    
    The answer is still human readable and contains more informations in more
    structurized way allowing to better understand the problem even when encountering
    plain text JSON answer.

--- presto-main/src/main/java/com/facebook/presto/server/StatementResource.java
+++ presto-main/src/main/java/com/facebook/presto/server/StatementResource.java
@@ -40,6 +40,7 @@ import com.facebook.presto.security.AccessControl;
 import com.facebook.presto.spi.ConnectorSession;
 import com.facebook.presto.spi.ErrorCode;
 import com.facebook.presto.spi.Page;
+import com.facebook.presto.spi.PrestoException;
 import com.facebook.presto.spi.QueryId;
 import com.facebook.presto.spi.block.Block;
 import com.facebook.presto.spi.type.StandardTypes;
@@ -171,14 +172,20 @@ public class StatementResource
     {
         assertRequest(!isNullOrEmpty(statement), "SQL statement is empty");
 
-        QuerySubmission querySubmission = parseStringToQuerySubmission(servletRequest, statement, querySubmissionCodec);
-        Session session = createSessionForRequest(servletRequest, accessControl, sessionPropertyManager, queryIdGenerator.createNextQueryId(), querySubmission.getPreparedStatements());
+        try {
+            QuerySubmission querySubmission = parseStringToQuerySubmission(servletRequest, statement, querySubmissionCodec);
+            Session session = createSessionForRequest(servletRequest, accessControl, sessionPropertyManager, queryIdGenerator.createNextQueryId(), querySubmission.getPreparedStatements());
 
-        ExchangeClient exchangeClient = exchangeClientSupplier.get(deltaMemoryInBytes -> { });
-        Query query = new Query(session, querySubmission.getQuery(), queryManager, exchangeClient);
-        queries.put(query.getQueryId(), query);
+            ExchangeClient exchangeClient = exchangeClientSupplier.get(deltaMemoryInBytes -> {
+            });
+            Query query = new Query(session, querySubmission.getQuery(), queryManager, exchangeClient);
+            queries.put(query.getQueryId(), query);
 
-        return getQueryResults(query, Optional.empty(), uriInfo, new Duration(1, MILLISECONDS), servletRequest);
+            return getQueryResults(query, Optional.empty(), uriInfo, new Duration(1, MILLISECONDS), servletRequest);
+        }
+        catch (PrestoException e) {
+            return Response.serverError().entity(e.toSerialized()).build();
+        }
     }
 
     @GET
