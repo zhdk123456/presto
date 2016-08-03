ee6515507f801bd82fc12b4fe88071f243e4c72e
Andrzej Fiedukowicz <Andrzej.Fiedukowicz@teradata.com>
2016-08-03 15:29:18 +0200

    Add CLI --quiet option allowing to remove tips from error messages

--- presto-main/src/main/java/com/facebook/presto/server/ResourceUtil.java
+++ presto-main/src/main/java/com/facebook/presto/server/ResourceUtil.java
@@ -1,3 +1,4 @@
+
 /*
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -219,6 +220,7 @@ final class ResourceUtil
                 sessionProperties,
                 preparedStatements,
                 transactionId,
+                false,
                 false,
                 clientRequestTimeout);
     }
