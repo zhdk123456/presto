42178cac8dab576b11979a1345e9cbe677662f77
Piotr Nowojski <piotr.nowojski@teradata.com>
2016-08-03 10:03:01 +0200

    Implement InMemory connector

--- presto-in-memory/src/main/java/com/facebook/presto/plugin/inmemory/InMemoryConnectorFactory.java
+++ presto-in-memory/src/main/java/com/facebook/presto/plugin/inmemory/InMemoryConnectorFactory.java
@@ -54,12 +54,13 @@ public class InMemoryConnectorFactory
     public Connector create(String connectorId, Map<String, String> requiredConfig, ConnectorContext context)
     {
         int splitsPerNode = getSplitsPerNode(requiredConfig);
+        InMemoryPagesStore pagesStore = new InMemoryPagesStore();
 
         return new InMemoryConnector(
-                new InMemoryMetadata(connectorId),
+                new InMemoryMetadata(connectorId, pagesStore),
                 new InMemorySplitManager(connectorId, context.getNodeManager(), splitsPerNode),
-                new InMemoryPageSourceProvider(),
-                new InMemoryPageSinkProvider());
+                new InMemoryPageSourceProvider(pagesStore),
+                new InMemoryPageSinkProvider(pagesStore));
     }
 
     private int getSplitsPerNode(Map<String, String> properties)
