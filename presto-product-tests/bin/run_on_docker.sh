7573c212aa957b1fb09b39f8434e610a1912b473
Andrzej Fiedukowicz <Andrzej.Fiedukowicz@teradata.com>
2016-08-04 13:51:22 +0200

    Make product-tests quiet about CLI errors

--- presto-product-tests/bin/run_on_docker.sh
+++ presto-product-tests/bin/run_on_docker.sh
@@ -45,6 +45,7 @@ function run_in_application_runner_container() {
 function check_presto() {
   run_in_application_runner_container \
     java -jar "/docker/volumes/presto-cli/presto-cli-executable.jar" \
+    --quiet \
     --server presto-master:8080 \
     --execute "SHOW CATALOGS" | grep -i hive
 }
