59f036c696362ee393d6a09626d0514a4367cf35
Rebecca Schlussel <rebecca.schlussel@teradata.com>
2016-08-31 15:12:49 -0400

    Add the data generation portion of the TPC-DS connector

--- presto-tpcds/src/main/java/com/facebook/presto/tpcds/TpcdsMetadata.java
+++ presto-tpcds/src/main/java/com/facebook/presto/tpcds/TpcdsMetadata.java
@@ -136,7 +136,7 @@ public class TpcdsMetadata
         ImmutableList.Builder<ColumnMetadata> columns = ImmutableList.builder();
         for (Column column : tpcdsTable.getColumns()) {
             columns.add(new ColumnMetadata(column.getName(), getPrestoType(column.getType())));
-    }
+        }
         SchemaTableName tableName = new SchemaTableName(schemaName, tpcdsTable.getName());
         return new ConnectorTableMetadata(tableName, columns.build());
     }
@@ -222,7 +222,7 @@ public class TpcdsMetadata
         }
     }
 
-    private static Type getPrestoType(ColumnType tpcdsType)
+    public static Type getPrestoType(ColumnType tpcdsType)
     {
         switch (tpcdsType.getBase()) {
             case IDENTIFIER:
