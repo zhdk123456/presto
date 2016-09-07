a22ffcf20e0c3a92a5ea3599f66925952df95edc
Piotr Nowojski <piotr.nowojski@teradata.com>
2016-09-07 10:31:04 +0200

    Implement BinaryFileSingleStreamSpiller

--- presto-main/src/main/java/com/facebook/presto/spiller/BinarySpillerFactory.java
+++ presto-main/src/main/java/com/facebook/presto/spiller/BinarySpillerFactory.java
@@ -71,4 +71,10 @@ public class BinarySpillerFactory
     {
         return new BinaryFileSpiller(serde.createPagesSerde(), executor, spillPath, totalSpilledBytes);
     }
+
+    @Override
+    public SingleStreamSpiller createSingleStreamSpiller(List<Type> types)
+    {
+        return new BinaryFileSingleStreamSpiller(serde.createPagesSerde(), executor, spillPath, totalSpilledBytes);
+    }
 }
