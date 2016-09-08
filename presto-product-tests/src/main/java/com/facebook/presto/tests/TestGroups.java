1976a2bbbf4f46bb557c03ca5e5cf94d6767b526
Anu Sudarsan <Anu.Sudarsan@teradata.com>
2016-09-07 22:49:53 -0400

    Add password authentication tests for LDAP

--- presto-product-tests/src/main/java/com/facebook/presto/tests/TestGroups.java
+++ presto-product-tests/src/main/java/com/facebook/presto/tests/TestGroups.java
@@ -54,6 +54,7 @@ public final class TestGroups
     public static final String CASSANDRA = "cassandra";
     public static final String TLS = "tls";
     public static final String LDAP = "ldap";
+    public static final String LDAP_CLI = "ldap_cli";
 
     private TestGroups() {}
 }
