f0ef75c8b50b8563af2d30c7c538c00ccfdd9c16
Karol Sobczak <Karol.Sobczak@Teradata.com>
2016-09-13 16:37:19 +0200

    fix q8 big query test (uses decimal now)

--- presto-product-tests/src/main/resources/sql-tests/testcases/hive_tpch/q8.sql
+++ presto-product-tests/src/main/resources/sql-tests/testcases/hive_tpch/q8.sql
@@ -1,4 +1,4 @@
--- database: presto; groups: tpch, big_query; tables: part,supplier,lineitem,orders,customer,nation
+-- database: presto; groups: tpch, big_query; tables: part,supplier,lineitem,orders,customer,nation,region
 SELECT
   o_year,
   sum(CASE
