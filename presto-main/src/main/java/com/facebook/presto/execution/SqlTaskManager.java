16117e921ae88639cedbb0d725e3abd1cc0771da
Lukasz Osipiuk <lukasz@osipiuk.net>
2016-09-13 19:25:45 +0200

    Periodically request memory revoking if needed

--- presto-main/src/main/java/com/facebook/presto/execution/SqlTaskManager.java
+++ presto-main/src/main/java/com/facebook/presto/execution/SqlTaskManager.java
@@ -88,6 +88,7 @@ public class SqlTaskManager
 
     private final LocalMemoryManager localMemoryManager;
     private final LocalSpillManager localSpillManager;
+    private final MemoryRevokingScheduler memoryRevokingScheduler;
     private final LoadingCache<QueryId, QueryContext> queryContexts;
     private final LoadingCache<TaskId, SqlTask> tasks;
 
@@ -107,6 +108,7 @@ public class SqlTaskManager
             QueryMonitor queryMonitor,
             NodeInfo nodeInfo,
             LocalMemoryManager localMemoryManager,
+            MemoryRevokingScheduler memoryRevokingScheduler,
             TaskManagerConfig config,
             NodeMemoryConfig nodeMemoryConfig,
             LocalSpillManager localSpillManager,
@@ -132,6 +134,8 @@ public class SqlTaskManager
 
         this.localSpillManager = requireNonNull(localSpillManager, "localSpillManager is null");
         DataSize maxQuerySpillPerNode = nodeSpillConfig.getQueryMaxSpillPerNode();
+
+        this.memoryRevokingScheduler = requireNonNull(memoryRevokingScheduler, "memoryRevokingScheduler can not be null");
 
         queryContexts = CacheBuilder.newBuilder().weakValues().build(new CacheLoader<QueryId, QueryContext>()
         {
@@ -212,6 +216,15 @@ public class SqlTaskManager
             }
             catch (Throwable e) {
                 log.warn(e, "Error updating stats");
+            }
+        }, 0, 1, TimeUnit.SECONDS);
+
+        taskManagementExecutor.scheduleWithFixedDelay(() -> {
+            try {
+                memoryRevokingScheduler.requestMemoryRevokingIfNeeded(tasks.asMap().values());
+            }
+            catch (Throwable e) {
+                log.warn(e, "Error requesting memory revoking");
             }
         }, 0, 1, TimeUnit.SECONDS);
     }
