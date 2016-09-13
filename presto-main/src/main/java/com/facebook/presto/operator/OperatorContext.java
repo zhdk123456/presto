8ce140b3f1761a5e310730b6cb5cdd2c44c877d5
Lukasz Osipiuk <lukasz@osipiuk.net>
2016-09-13 19:25:45 +0200

    Periodically request memory revoking if needed

--- presto-main/src/main/java/com/facebook/presto/operator/OperatorContext.java
+++ presto-main/src/main/java/com/facebook/presto/operator/OperatorContext.java
@@ -275,7 +275,7 @@ public class OperatorContext
         memoryReservation.getAndAdd(-bytes);
     }
 
-    public AbstractAggregatedMemoryContext getSystemMemoryContext()
+    public OperatorSystemMemoryContext getSystemMemoryContext()
     {
         return systemMemoryContext;
     }
@@ -489,7 +489,7 @@ public class OperatorContext
         }
     }
 
-    private static class OperatorSystemMemoryContext
+    public static class OperatorSystemMemoryContext
             extends AbstractAggregatedMemoryContext
     {
         // TODO: remove this class. See comment in AbstractAggregatedMemoryContext
