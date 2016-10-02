4d7814b7aed03325b51724cb81616ad77aa0b0c9
Lukasz Osipiuk <lukasz@osipiuk.net>
2016-10-02 21:48:49 +0200

    Allow multiple files per bucket in hive
    By default Presto supports only one data file per bucket per partition
    for clustered tables (Hive tables declared with ``CLUSTERED BY``
    clause).  If number of files does not match number of buckets exception
    would be thrown.
    
    To enable support for cases where there are more than one file per
    bucket, when multiple INSERTs were done to a single partition of the
    clustered table, you can use:
    
     * ``hive.multi-file-bucketing.enabled`` config property
     * ``multi_file_bucketing_enabled`` session property
    
    Config property changes behaviour globally and session property can be
    used on per query basis.  The default value of session property is
    taken from config property.
    
    If support for multiple files per bucket is enabled Presto will group
    the files in partition directory.  It will sort filenames
    lexicographically. Then it will treat part of filename up to first
    underscore character as bucket key.
    This pattern matches naming convention of files in directory when Hive
    is used to inject data into table.
    
    Presto will still validate if number of file groups matches number of
    buckets declared for table and fail if it does not.

--- presto-hive/src/test/java/com/facebook/presto/hive/TestHiveClientConfig.java
+++ presto-hive/src/test/java/com/facebook/presto/hive/TestHiveClientConfig.java
@@ -88,7 +88,8 @@ public class TestHiveClientConfig
                 .setSkipDeletionForAlter(false)
                 .setBucketExecutionEnabled(true)
                 .setBucketWritingEnabled(true)
-                .setFileSystemMaxCacheSize(1000));
+                .setFileSystemMaxCacheSize(1000)
+                .setMultiFileBucketingEnabled(false));
     }
 
     @Test
@@ -150,6 +151,7 @@ public class TestHiveClientConfig
                 .put("hive.bucket-execution", "false")
                 .put("hive.bucket-writing", "false")
                 .put("hive.fs.cache.max-size", "1010")
+                .put("hive.multi-file-bucketing.enabled", "true")
                 .build();
 
         HiveClientConfig expected = new HiveClientConfig()
@@ -207,7 +209,8 @@ public class TestHiveClientConfig
                 .setSkipDeletionForAlter(true)
                 .setBucketExecutionEnabled(false)
                 .setBucketWritingEnabled(false)
-                .setFileSystemMaxCacheSize(1010);
+                .setFileSystemMaxCacheSize(1010)
+                .setMultiFileBucketingEnabled(true);
 
         ConfigAssertions.assertFullMapping(properties, expected);
     }
