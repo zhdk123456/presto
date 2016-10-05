33e2b450ff21b6692e07be35e58cc3eac512e7e3
Christina Wallin <christina.wallin@teradata.com>
2016-10-05 10:47:58 -0400

    Rename spilledBytes to totalSpilledBytes

--- presto-main/src/main/java/com/facebook/presto/operator/HashAggregationOperator.java
+++ presto-main/src/main/java/com/facebook/presto/operator/HashAggregationOperator.java
@@ -105,8 +105,7 @@ public class HashAggregationOperator
                             throw new UnsupportedOperationException();
                         }
 
-                        @Override
-                        public long getSpilledBytes()
+                        public long getTotalSpilledBytes()
                         {
                             return 0;
                         }
