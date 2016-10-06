4ac26cfa1b74f67b8bda7cbc42875808576b60af
Rebecca Schlussel <rebecca.schlussel@teradata.com>
2016-10-06 13:38:33 -0400

    Add join method as a field to SemiJoinNode
    Indicates whether a semi-join is distributed or broadcast.
    Necessary to be able to add an optimizer that chooses between the two.

--- presto-main/src/test/java/com/facebook/presto/sql/planner/TestEffectivePredicateExtractor.java
+++ presto-main/src/test/java/com/facebook/presto/sql/planner/TestEffectivePredicateExtractor.java
@@ -718,6 +718,7 @@ public class TestEffectivePredicateExtractor
                 filter(baseTableScan, greaterThan(AE, bigintLiteral(5))),
                 A, B, C,
                 Optional.empty(),
+                Optional.empty(),
                 Optional.empty());
 
         Expression effectivePredicate = EffectivePredicateExtractor.extract(node, TYPES);
