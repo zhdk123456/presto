bec03c1cf4d550afd18a357533b752a14688e5aa
Rebecca Schlussel <rebecca.schlussel@teradata.com>
2016-10-06 13:38:33 -0400

    Add join method as a field to SemiJoinNode
    Indicates whether a semi-join is distributed or broadcast.
    Necessary to be able to add an optimizer that chooses between the two.

--- presto-main/src/main/java/com/facebook/presto/sql/planner/optimizations/PruneUnreferencedOutputs.java
+++ presto-main/src/main/java/com/facebook/presto/sql/planner/optimizations/PruneUnreferencedOutputs.java
@@ -254,7 +254,8 @@ public class PruneUnreferencedOutputs
                     node.getFilteringSourceJoinSymbol(),
                     node.getSemiJoinOutput(),
                     node.getSourceHashSymbol(),
-                    node.getFilteringSourceHashSymbol());
+                    node.getFilteringSourceHashSymbol(),
+                    node.getDistributionType());
         }
 
         @Override
