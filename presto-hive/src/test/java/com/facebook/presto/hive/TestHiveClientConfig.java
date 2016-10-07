6a58e648e57e25b58e2025def0705d47633bdec2
Lukasz Osipiuk <lukasz@osipiuk.net>
2016-10-07 14:53:22 +0200

    Add tests for bucketed table with empty partitions

--- presto-hive/src/test/java/com/facebook/presto/hive/TestHiveClientConfig.java
+++ presto-hive/src/test/java/com/facebook/presto/hive/TestHiveClientConfig.java
@@ -105,6 +105,7 @@ public class TestHiveClientConfig
                 .setBucketExecutionEnabled(true)
                 .setForceIntegralToBigint(false)
                 .setBucketWritingEnabled(true)
+                .setMultiFileBucketingEnabled(false)
                 .setEmptyBucketedPartitionsEnabled(false));
     }
 
