ea8282b183ebed27f70db4c1ed9f210a1ea0d05e
Piotr Nowojski <piotr.nowojski@teradata.com>
2016-10-13 13:16:58 +0200

    Revert "Lazy int columns - no speed up"
    This reverts commit 30c4ffd28e0189a4b52951cfd71b81d7e1ac5040.

--- presto-main/src/main/java/com/facebook/presto/row/TestRowObject.java
+++ presto-main/src/main/java/com/facebook/presto/row/TestRowObject.java
@@ -12,17 +12,17 @@ public class TestRowObject
     public int pos0;
     public Block col0;
     public long col1;
-    public Block col2;
+    public long col2;
 
     public int pos3;
     public Block col3;
-    public Block col4;
-    public Block col5;
+    public long col4;
+    public long col5;
 
     public int pos6;
     public Block col6;
-    public Block col7;
-    public Block col8;
+    public long col7;
+    public long col8;
 
     @Override
     public void appendTo(PageBuilder pageBuilder)
@@ -30,34 +30,35 @@ public class TestRowObject
         pageBuilder.declarePosition();
         VarcharType.VARCHAR.appendTo(col0, pos0, pageBuilder.getBlockBuilder(0));
         pageBuilder.getBlockBuilder(1).writeLong(col1);
-        BigintType.BIGINT.appendTo(col2, pos0, pageBuilder.getBlockBuilder(2));
+        pageBuilder.getBlockBuilder(2).writeLong(col2);
+
 
         VarcharType.VARCHAR.appendTo(col3, pos3, pageBuilder.getBlockBuilder(3));
-        BigintType.BIGINT.appendTo(col4, pos3, pageBuilder.getBlockBuilder(4));
-        BigintType.BIGINT.appendTo(col5, pos3, pageBuilder.getBlockBuilder(5));
+        pageBuilder.getBlockBuilder(4).writeLong(col4);
+        pageBuilder.getBlockBuilder(5).writeLong(col5);
 
         VarcharType.VARCHAR.appendTo(col6, pos6, pageBuilder.getBlockBuilder(6));
-        BigintType.BIGINT.appendTo(col7, pos6, pageBuilder.getBlockBuilder(7));
-        BigintType.BIGINT.appendTo(col8, pos6, pageBuilder.getBlockBuilder(8));
+        pageBuilder.getBlockBuilder(7).writeLong(col7);
+        pageBuilder.getBlockBuilder(8).writeLong(col8);
     }
 
     public void set(int id, int blockPosition, Block block0, Block block1, Block block2)
     {
         //Slice slice = VarcharType.VARCHAR.getSlice(block0, blockPosition);
-        //long aLong0 = BigintType.BIGINT.getLong(block1, blockPosition);
-        //long aLong1 = BigintType.BIGINT.getLong(block2, blockPosition);
+        long aLong0 = BigintType.BIGINT.getLong(block1, blockPosition);
+        long aLong1 = BigintType.BIGINT.getLong(block2, blockPosition);
         switch (id) {
             case 0:
                 pos3 = blockPosition;
                 col3 = block0;
-                col4 = block1;
-                col5 = block2;
+                col4 = aLong0;
+                col5 = aLong1;
                 break;
             case 1:
                 pos6 = blockPosition;
                 col6 = block0;
-                col7 = block1;
-                col8 = block2;
+                col7 = aLong0;
+                col8 = aLong1;
                 break;
             default:
                 throw new IllegalStateException();
