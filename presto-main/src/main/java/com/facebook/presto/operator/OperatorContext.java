71ec7fb4a623418d916f19d1960a2b6a597fb4a1
Christina Wallin <christina.wallin@teradata.com>
2016-10-18 13:48:37 -0400

    Add an OperatorSpillContext

--- presto-main/src/main/java/com/facebook/presto/operator/OperatorContext.java
+++ presto-main/src/main/java/com/facebook/presto/operator/OperatorContext.java
@@ -87,6 +87,8 @@ public class OperatorContext
     private final AtomicLong revocableMemoryReservation = new AtomicLong();
     private final OperatorSystemMemoryContext systemMemoryContext;
 
+    private final OperatorSpillContext spillContext;
+
     private final AtomicReference<Supplier<?>> infoSupplier = new AtomicReference<>();
     private final boolean collectTimings;
 
@@ -101,6 +103,7 @@ public class OperatorContext
         this.operatorType = requireNonNull(operatorType, "operatorType is null");
         this.driverContext = requireNonNull(driverContext, "driverContext is null");
         this.systemMemoryContext = new OperatorSystemMemoryContext(this.driverContext);
+        this.spillContext = new OperatorSpillContext(this.driverContext);
         this.executor = requireNonNull(executor, "executor is null");
 
         this.memoryFuture = new AtomicReference<>(SettableFuture.create());
@@ -316,6 +319,11 @@ public class OperatorContext
     public void closeSystemMemoryContext()
     {
         systemMemoryContext.close();
+    }
+
+    public OperatorSpillContext getSpillContext()
+    {
+        return spillContext;
     }
 
     public void moreMemoryAvailable()
@@ -568,6 +576,40 @@ public class OperatorContext
             return toStringHelper(this)
                     .add("usedBytes", reservedBytes)
                     .add("closed", closed)
+                    .toString();
+        }
+    }
+
+    private class OperatorSpillContext
+        implements AbstractOperatorSpillContext
+    {
+        private final DriverContext driverContext;
+
+        private long reservedBytes;
+
+        public OperatorSpillContext(DriverContext driverContext)
+        {
+            this.driverContext = driverContext;
+        }
+
+        @Override
+        public void updateBytes(long bytes)
+        {
+            if (bytes > 0) {
+                driverContext.reserveSpill(bytes);
+            }
+            else {
+                checkArgument(reservedBytes + bytes >= 0, "tried to free %s spilled bytes from %s bytes reserved", -bytes, reservedBytes);
+                driverContext.freeSpill(-bytes);
+            }
+            reservedBytes += bytes;
+        }
+
+        @Override
+        public String toString()
+        {
+            return toStringHelper(this)
+                    .add("usedBytes", reservedBytes)
                     .toString();
         }
     }
