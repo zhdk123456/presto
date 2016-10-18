8424bee9c65a1af28c5c2038b8830b26477a30e5
Andrii Rosa <Andriy.Rosa@TERADATA.COM>
2016-10-18 10:49:16 -0400

    Implement LDAP authentication for internal communication

--- presto-main/src/main/java/com/facebook/presto/server/ServerMainModule.java
+++ presto-main/src/main/java/com/facebook/presto/server/ServerMainModule.java
@@ -123,6 +123,7 @@ import com.google.inject.TypeLiteral;
 import io.airlift.concurrent.BoundedExecutor;
 import io.airlift.configuration.AbstractConfigurationAwareModule;
 import io.airlift.discovery.client.ServiceDescriptor;
+import io.airlift.http.client.BasicAuthRequestFilter;
 import io.airlift.http.client.HttpClientConfig;
 import io.airlift.node.NodeInfo;
 import io.airlift.slice.Slice;
@@ -138,6 +139,7 @@ import java.util.concurrent.ScheduledExecutorService;
 
 import static com.facebook.presto.execution.scheduler.NodeSchedulerConfig.NetworkTopologyType.FLAT;
 import static com.facebook.presto.execution.scheduler.NodeSchedulerConfig.NetworkTopologyType.LEGACY;
+import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkState;
 import static com.google.common.base.Strings.nullToEmpty;
 import static com.google.common.reflect.Reflection.newProxy;
@@ -192,6 +194,13 @@ public class ServerMainModule
             config.setKeyStorePath(internalCommunicationConfig.getKeyStorePath());
             config.setKeyStorePassword(internalCommunicationConfig.getKeyStorePassword());
         });
+
+        String ldapUser = internalCommunicationConfig.getLdapUser();
+        if (ldapUser != null) {
+            String ldapPassword = internalCommunicationConfig.getLdapPassword();
+            checkArgument(ldapPassword != null, "ldap password must be set");
+            httpClientBinder(binder).bindGlobalFilter(new BasicAuthRequestFilter(ldapUser, ldapPassword));
+        }
 
         configBinder(binder).bindConfig(FeaturesConfig.class);
 
