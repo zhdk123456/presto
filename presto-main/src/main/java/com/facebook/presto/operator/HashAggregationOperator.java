e7bf34ddbb758d572ea627343970e19f2dc181da
Piotr Nowojski <piotr.nowojski@teradata.com>
2016-10-19 14:11:48 +0200

    Implement isBlocked in HashAggregationOperator
    Fixed perfomance issue with spilling queries being blocked for too long time

--- presto-main/src/main/java/com/facebook/presto/operator/HashAggregationOperator.java
+++ presto-main/src/main/java/com/facebook/presto/operator/HashAggregationOperator.java
@@ -28,6 +28,7 @@ import com.facebook.presto.sql.planner.plan.PlanNodeId;
 import com.facebook.presto.type.TypeUtils;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.ImmutableList;
+import com.google.common.util.concurrent.ListenableFuture;
 import io.airlift.units.DataSize;
 
 import java.util.Iterator;
@@ -37,6 +38,7 @@ import java.util.stream.Collectors;
 
 import static com.facebook.presto.operator.aggregation.builder.InMemoryHashAggregationBuilder.toTypes;
 import static com.google.common.base.Preconditions.checkState;
+import static io.airlift.concurrent.MoreFutures.toListenableFuture;
 import static io.airlift.units.DataSize.Unit.MEGABYTE;
 import static java.util.Objects.requireNonNull;
 
@@ -323,12 +325,21 @@ public class HashAggregationOperator
     }
 
     @Override
+    public ListenableFuture<?> isBlocked()
+    {
+        if (aggregationBuilder != null) {
+            return toListenableFuture(aggregationBuilder.isBlocked());
+        }
+        return NOT_BLOCKED;
+    }
+
+    @Override
     public boolean needsInput()
     {
         if (finishing || outputIterator != null) {
             return false;
         }
-        else if (aggregationBuilder != null && (aggregationBuilder.isFull() || aggregationBuilder.isBusy())) {
+        else if (aggregationBuilder != null && aggregationBuilder.isFull()) {
             return false;
         }
         else {
@@ -404,11 +415,6 @@ public class HashAggregationOperator
 
             // only flush if we are finishing or the aggregation builder is full
             if (!finishing && (aggregationBuilder == null || !aggregationBuilder.isFull())) {
-                return null;
-            }
-
-            // don't ask for output if builder is busy
-            if (aggregationBuilder.isBusy()) {
                 return null;
             }
 
