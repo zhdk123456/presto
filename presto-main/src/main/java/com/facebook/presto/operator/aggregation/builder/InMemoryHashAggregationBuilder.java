701ccd5f8d0b8e88fe88424cbef97f579dedc16a
Piotr Nowojski <piotr.nowojski@teradata.com>
2016-10-19 14:11:48 +0200

    Remove isBlocked from in HashAggregationBuilder
    Future returned from startMemoryRevoke ensures that operator will
    not be executed until spill completes so isBlocked is no longer needed.

--- presto-main/src/main/java/com/facebook/presto/operator/aggregation/builder/InMemoryHashAggregationBuilder.java
+++ presto-main/src/main/java/com/facebook/presto/operator/aggregation/builder/InMemoryHashAggregationBuilder.java
@@ -46,7 +46,6 @@ import static com.facebook.presto.operator.GroupByHash.createGroupByHash;
 import static com.facebook.presto.spi.type.BigintType.BIGINT;
 import static com.google.common.base.Preconditions.checkArgument;
 import static java.util.Objects.requireNonNull;
-import static java.util.concurrent.CompletableFuture.completedFuture;
 
 public class InMemoryHashAggregationBuilder
         implements HashAggregationBuilder
@@ -168,12 +167,6 @@ public class InMemoryHashAggregationBuilder
     public double getExpectedHashCollisions()
     {
         return groupByHash.getExpectedHashCollisions();
-    }
-
-    @Override
-    public CompletableFuture<?> isBlocked()
-    {
-        return completedFuture(null);
     }
 
     @Override
