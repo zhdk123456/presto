b707c5ff21c62f29c57ce4cbde98c2532a403776
Lukasz Osipiuk <lukasz@osipiuk.net>
2016-10-19 17:17:07 +0200

    Do not use Hive to populate tables in TestHiveBucketedTables
    This patch changes how tables are populated in TestHiveBucketedTables.
    Instead using hive we simulate its behaviour and put files directly into
    HDFS.
    
    This change is needed as populating tables with hive is to slow and
    results in intermittent test failures.

--- presto-product-tests/src/main/java/com/facebook/presto/tests/hive/TestHiveBucketedTables.java
+++ presto-product-tests/src/main/java/com/facebook/presto/tests/hive/TestHiveBucketedTables.java
@@ -13,6 +13,7 @@
  */
 package com.facebook.presto.tests.hive;
 
+import com.google.inject.name.Named;
 import com.teradata.tempto.ProductTest;
 import com.teradata.tempto.Requirement;
 import com.teradata.tempto.Requirements;
@@ -21,22 +22,22 @@ import com.teradata.tempto.configuration.Configuration;
 import com.teradata.tempto.fulfillment.table.MutableTableRequirement;
 import com.teradata.tempto.fulfillment.table.TableDefinitionsRepository;
 import com.teradata.tempto.fulfillment.table.hive.HiveTableDefinition;
+import com.teradata.tempto.hadoop.hdfs.HdfsClient;
 import com.teradata.tempto.query.QueryExecutionException;
 import org.testng.annotations.Test;
+
+import javax.inject.Inject;
 
 import java.sql.SQLException;
 import java.util.Optional;
 
 import static com.facebook.presto.tests.TestGroups.HIVE_CONNECTOR;
-import static com.facebook.presto.tests.TpchTableResults.PRESTO_NATION_RESULT;
 import static com.facebook.presto.tests.utils.JdbcDriverUtils.setSessionProperty;
 import static com.facebook.presto.tests.utils.QueryExecutors.onHive;
 import static com.facebook.presto.tests.utils.TableDefinitionUtils.mutableTableInstanceOf;
 import static com.teradata.tempto.assertions.QueryAssert.Row.row;
 import static com.teradata.tempto.assertions.QueryAssert.assertThat;
 import static com.teradata.tempto.fulfillment.table.MutableTableRequirement.State.CREATED;
-import static com.teradata.tempto.fulfillment.table.TableRequirements.immutableTable;
-import static com.teradata.tempto.fulfillment.table.hive.tpch.TpchTableDefinitions.NATION;
 import static com.teradata.tempto.query.QueryExecutor.defaultQueryExecutor;
 import static com.teradata.tempto.query.QueryExecutor.query;
 import static java.lang.String.format;
@@ -45,27 +46,41 @@ public class TestHiveBucketedTables
         extends ProductTest
         implements RequirementsProvider
 {
-    @TableDefinitionsRepository.RepositoryTableDefinition
-    public static final HiveTableDefinition BUCKETED_NATION = bucketTableDefinition("bucket_nation", false, false);
-    @TableDefinitionsRepository.RepositoryTableDefinition
-    public static final HiveTableDefinition BUCKETED_EMPTY_NATION = bucketTableDefinition("bucket_empty_nation", false, false);
-    @TableDefinitionsRepository.RepositoryTableDefinition
-    public static final HiveTableDefinition BUCKETED_SORTED_NATION = bucketTableDefinition("bucket_sort_nation", true, false);
-    @TableDefinitionsRepository.RepositoryTableDefinition
-    public static final HiveTableDefinition BUCKETED_PARTITIONED_NATION = bucketTableDefinition("bucket_partition_nation", false, true);
+    @Inject
+    private HdfsClient hdfsClient;
 
-    private static HiveTableDefinition bucketTableDefinition(String tableName, boolean sorted, boolean partitioned)
+    @Inject
+    @Named("databases.hive.warehouse_directory_path")
+    private String warehouseDirectoryPath;
+
+    @TableDefinitionsRepository.RepositoryTableDefinition
+    public static final HiveTableDefinition BUCKETED_TABLE = bucketTableDefinition("bucket_table", false);
+    @TableDefinitionsRepository.RepositoryTableDefinition
+    public static final HiveTableDefinition BUCKETED_EMPTY_TABLE = bucketTableDefinition("bucket_empty_table", false);
+    @TableDefinitionsRepository.RepositoryTableDefinition
+    public static final HiveTableDefinition BUCKETED_PARTITIONED_TABLE = bucketTableDefinition("bucket_partition_table", true);
+
+    private static final String BUCKET_0_CONTENTS = "0|a\n2|c\n4|e\n6|g\n";
+    private static final String BUCKET_1_CONTENTS = "1|b\n3|d\n5|f\n";
+
+    private String getTableLocation(String tableName, Optional<String> partitionName)
+    {
+        String location = warehouseDirectoryPath + '/' + tableName;
+        if (partitionName.isPresent()) {
+            location += "/" + partitionName.get();
+        }
+        return location;
+    }
+
+    private static HiveTableDefinition bucketTableDefinition(String tableName, boolean partitioned)
     {
         return HiveTableDefinition.builder(tableName)
                 .setCreateTableDDLTemplate("CREATE TABLE %NAME%(" +
-                        "n_nationkey     BIGINT," +
-                        "n_name          STRING," +
-                        "n_regionkey     BIGINT," +
-                        "n_comment       STRING) " +
-                        (partitioned ? "PARTITIONED BY (part_key STRING) " : " ") +
-                        "CLUSTERED BY (n_regionkey) " +
-                        (sorted ? "SORTED BY (n_regionkey) " : " ") +
-                        "INTO 4 BUCKETS " +
+                        "a     BIGINT, " +
+                        "b     STRING) " +
+                        (partitioned ? "PARTITIONED BY (c BIGINT) " : " ") +
+                        "CLUSTERED BY (a) " +
+                        "INTO 2 BUCKETS " +
                         "ROW FORMAT DELIMITED FIELDS TERMINATED BY '|'"
                 )
                 .setNoData()
@@ -76,25 +91,28 @@ public class TestHiveBucketedTables
     public Requirement getRequirements(Configuration configuration)
     {
         return Requirements.compose(
-                MutableTableRequirement.builder(BUCKETED_NATION).withState(CREATED).build(),
-                MutableTableRequirement.builder(BUCKETED_EMPTY_NATION).withState(CREATED).build(),
-                MutableTableRequirement.builder(BUCKETED_PARTITIONED_NATION).withState(CREATED).build(),
-                MutableTableRequirement.builder(BUCKETED_SORTED_NATION).withState(CREATED).build(),
-                immutableTable(NATION));
+                MutableTableRequirement.builder(BUCKETED_TABLE).withState(CREATED).build(),
+                MutableTableRequirement.builder(BUCKETED_EMPTY_TABLE).withState(CREATED).build(),
+                MutableTableRequirement.builder(BUCKETED_PARTITIONED_TABLE).withState(CREATED).build());
     }
 
     @Test(groups = {HIVE_CONNECTOR})
     public void testSelectStar()
             throws SQLException
     {
-        String tableName = mutableTableInstanceOf(BUCKETED_NATION).getNameInDatabase();
-        populateDataToHiveTable(tableName, NATION.getName(), Optional.empty());
+        String tableName = mutableTableInstanceOf(BUCKETED_TABLE).getNameInDatabase();
+        populateDataToHiveTable(tableName, Optional.empty(), Optional.empty());
 
-        assertThat(query(format("SELECT * FROM %s", tableName))).matches(PRESTO_NATION_RESULT);
-        assertThat(query(format("SELECT count(*) FROM %s WHERE n_nationkey = 1", tableName)))
+        assertThat(query(format("SELECT * FROM %s WHERE a < 4", tableName))).containsOnly(
+                row(0, "a"),
+                row(1, "b"),
+                row(2, "c"),
+                row(3, "d")
+        );
+        assertThat(query(format("SELECT count(*) FROM %s WHERE a = 1", tableName)))
                 .containsExactly(row(1));
-        assertThat(query(format("SELECT count(*) FROM %s n JOIN %s n1 ON n.n_regionkey = n1.n_regionkey", tableName, tableName)))
-                .containsExactly(row(125));
+        assertThat(query(format("SELECT count(*) FROM %s t1 JOIN %s t2 ON t1.a = t2.a", tableName, tableName)))
+                .containsExactly(row(7));
     }
 
     @Test(groups = {HIVE_CONNECTOR},
@@ -103,78 +121,61 @@ public class TestHiveBucketedTables
     public void testSelectAfterMultipleInsertsMultiBucketDisabled()
             throws SQLException
     {
-        String tableName = mutableTableInstanceOf(BUCKETED_NATION).getNameInDatabase();
-        populateDataToHiveTable(tableName, NATION.getName(), Optional.empty());
-        populateDataToHiveTable(tableName, NATION.getName(), Optional.empty());
+        String tableName = mutableTableInstanceOf(BUCKETED_TABLE).getNameInDatabase();
+        populateDataToHiveTable(tableName, Optional.empty(), Optional.empty());
+        populateDataToHiveTable(tableName, Optional.empty(), Optional.of("_copy_0"));
 
-        query(format("SELECT count(*) FROM %s WHERE n_nationkey = 1", tableName));
+        query(format("SELECT count(*) FROM %s WHERE a = 1", tableName));
     }
 
     @Test(groups = {HIVE_CONNECTOR})
     public void testSelectAfterMultipleInserts()
             throws SQLException
     {
-        String tableName = mutableTableInstanceOf(BUCKETED_NATION).getNameInDatabase();
-        populateDataToHiveTable(tableName, NATION.getName(), Optional.empty());
-        populateDataToHiveTable(tableName, NATION.getName(), Optional.empty());
+        String tableName = mutableTableInstanceOf(BUCKETED_TABLE).getNameInDatabase();
+        populateDataToHiveTable(tableName, Optional.empty(), Optional.empty());
+        populateDataToHiveTable(tableName, Optional.empty(), Optional.of("_copy_0"));
 
         enableMultiFileBucketing();
 
-        assertThat(query(format("SELECT count(*) FROM %s WHERE n_nationkey = 1", tableName)))
+        assertThat(query(format("SELECT count(*) FROM %s WHERE a = 1", tableName)))
                 .containsExactly(row(2));
-        assertThat(query(format("SELECT count(*) FROM %s WHERE n_regionkey = 1", tableName)))
-                .containsExactly(row(10));
-        assertThat(query(format("SELECT n_regionkey, count(*) FROM %s GROUP BY n_regionkey", tableName)))
-                .containsOnly(row(0, 10), row(1, 10), row(2, 10), row(3, 10), row(4, 10));
-        assertThat(query(format("SELECT count(*) FROM %s n JOIN %s n1 ON n.n_regionkey = n1.n_regionkey", tableName, tableName)))
-                .containsExactly(row(500));
-    }
-
-    @Test(groups = {HIVE_CONNECTOR})
-    public void testSelectAfterMultipleInsertsForSortedTable()
-            throws SQLException
-    {
-        String tableName = mutableTableInstanceOf(BUCKETED_SORTED_NATION).getNameInDatabase();
-        populateDataToHiveTable(tableName, NATION.getName(), Optional.empty());
-        populateDataToHiveTable(tableName, NATION.getName(), Optional.empty());
-
-        enableMultiFileBucketing();
-
-        assertThat(query(format("SELECT count(*) FROM %s WHERE n_nationkey = 1", tableName)))
-                .containsExactly(row(2));
-        assertThat(query(format("SELECT count(*) FROM %s WHERE n_regionkey = 1", tableName)))
-                .containsExactly(row(10));
-        assertThat(query(format("SELECT n_regionkey, count(*) FROM %s GROUP BY n_regionkey", tableName)))
-                .containsOnly(row(0, 10), row(1, 10), row(2, 10), row(3, 10), row(4, 10));
-        assertThat(query(format("SELECT count(*) FROM %s n JOIN %s n1 ON n.n_regionkey = n1.n_regionkey", tableName, tableName)))
-                .containsExactly(row(500));
+        assertThat(query(format("SELECT count(*) FROM %s WHERE a < 4", tableName)))
+                .containsExactly(row(8));
+        assertThat(query(format("SELECT a, count(*) FROM %s GROUP BY a", tableName)))
+                .containsOnly(row(0, 2), row(1, 2), row(2, 2), row(3, 2), row(4, 2), row(5, 2), row(6, 2));
+        assertThat(query(format("SELECT count(*) FROM %s 1 JOIN %s t1 ON t1.a = t2.a", tableName, tableName)))
+                .containsExactly(row(28));
     }
 
     @Test(groups = {HIVE_CONNECTOR})
     public void testSelectAfterMultipleInsertsForPartitionedTable()
             throws SQLException
     {
-        String tableName = mutableTableInstanceOf(BUCKETED_PARTITIONED_NATION).getNameInDatabase();
-        populateDataToHiveTable(tableName, NATION.getName(), Optional.of("part_key = 'insert_1'"));
-        populateDataToHiveTable(tableName, NATION.getName(), Optional.of("part_key = 'insert_2'"));
-        populateDataToHiveTable(tableName, NATION.getName(), Optional.of("part_key = 'insert_1'"));
-        populateDataToHiveTable(tableName, NATION.getName(), Optional.of("part_key = 'insert_2'"));
+        String tableName = mutableTableInstanceOf(BUCKETED_PARTITIONED_TABLE).getNameInDatabase();
+
+        onHive().executeQuery(format("ALTER TABLE %s ADD PARTITION (c=1)", tableName));
+        onHive().executeQuery(format("ALTER TABLE %s ADD PARTITION (c=2)", tableName));
+
+        populateDataToHiveTable(tableName, Optional.of("c=1"), Optional.empty());
+        populateDataToHiveTable(tableName, Optional.of("c=2"), Optional.empty());
+        populateDataToHiveTable(tableName, Optional.of("c=1"), Optional.of("_copy_0"));
+        populateDataToHiveTable(tableName, Optional.of("c=2"), Optional.of("_copy_0"));
 
         enableMultiFileBucketing();
 
-        assertThat(query(format("SELECT count(*) FROM %s WHERE n_nationkey = 1", tableName)))
+        assertThat(query(format("SELECT count(*) FROM %s WHERE a = 1", tableName)))
                 .containsExactly(row(4));
-        assertThat(query(format("SELECT count(*) FROM %s WHERE n_regionkey = 1", tableName)))
-                .containsExactly(row(20));
-        assertThat(query(format("SELECT count(*) FROM %s WHERE n_regionkey = 1 AND part_key = 'insert_1'", tableName)))
-                .hasRowsCount(1)
-                .containsExactly(row(10));
-        assertThat(query(format("SELECT n_regionkey, count(*) FROM %s WHERE part_key = 'insert_2' GROUP BY n_regionkey", tableName)))
-                .containsOnly(row(0, 10), row(1, 10), row(2, 10), row(3, 10), row(4, 10));
-        assertThat(query(format("SELECT count(*) FROM %s n JOIN %s n1 ON n.n_regionkey = n1.n_regionkey", tableName, tableName)))
-                .containsExactly(row(2000));
-        assertThat(query(format("SELECT count(*) FROM %s n JOIN %s n1 ON n.n_regionkey = n1.n_regionkey WHERE n.part_key = 'insert_1'", tableName, tableName)))
-                .containsExactly(row(1000));
+        assertThat(query(format("SELECT count(*) FROM %s WHERE b = 'a'", tableName)))
+                .containsExactly(row(4));
+        assertThat(query(format("SELECT count(*) FROM %s WHERE a = 1 AND c = 1", tableName)))
+                .containsExactly(row(2));
+        assertThat(query(format("SELECT a, count(*) FROM %s WHERE c = 2 GROUP BY a", tableName)))
+                .containsOnly(row(0, 2), row(1, 2), row(2, 2), row(3, 2), row(4, 2), row(5, 2), row(6, 2));
+        assertThat(query(format("SELECT count(*) FROM %s t1 JOIN %s t2 ON t1.a = t2.a", tableName, tableName)))
+                .containsExactly(row(4 * 4 * 7));
+        assertThat(query(format("SELECT count(*) FROM %s t1 JOIN %s t2 ON t1.a = t2.a WHERE t1.c = 1", tableName, tableName)))
+                .containsExactly(row(4 * 4 * 7 / 2));
     }
 
     @Test(groups = {HIVE_CONNECTOR},
@@ -183,7 +184,7 @@ public class TestHiveBucketedTables
     public void testSelectFromEmptyBucketedTableEmptyTablesNotAllowed()
             throws SQLException
     {
-        String tableName = mutableTableInstanceOf(BUCKETED_EMPTY_NATION).getNameInDatabase();
+        String tableName = mutableTableInstanceOf(BUCKETED_EMPTY_TABLE).getNameInDatabase();
         query(format("SELECT count(*) FROM %s", tableName));
     }
 
@@ -191,7 +192,7 @@ public class TestHiveBucketedTables
     public void testSelectFromEmptyBucketedTableEmptyTablesAllowed()
             throws SQLException
     {
-        String tableName = mutableTableInstanceOf(BUCKETED_EMPTY_NATION).getNameInDatabase();
+        String tableName = mutableTableInstanceOf(BUCKETED_EMPTY_TABLE).getNameInDatabase();
         enableEmptyBucketedPartitions();
         assertThat(query(format("SELECT count(*) FROM %s", tableName)))
                 .containsExactly(row(0));
@@ -209,15 +210,9 @@ public class TestHiveBucketedTables
         setSessionProperty(defaultQueryExecutor().getConnection(), "hive.empty_bucketed_partitions_enabled", "true");
     }
 
-    private static void populateDataToHiveTable(String destination, String source, Optional<String> partition)
+    private void populateDataToHiveTable(String tableName, Optional<String> partitionName, Optional<String> fileNameSuffix)
     {
-        String queryStatement = format("INSERT INTO %s" +
-                        (partition.isPresent() ? format(" PARTITION (%s) ", partition.get()) : " ") +
-                        "SELECT * FROM %s",
-                destination, source);
-
-        onHive().executeQuery("set hive.enforce.bucketing = true");
-        onHive().executeQuery("set hive.enforce.sorting = true");
-        onHive().executeQuery(queryStatement);
+        hdfsClient.saveFile(getTableLocation(tableName, partitionName) + "/000000_0" + fileNameSuffix.orElse(""), BUCKET_0_CONTENTS);
+        hdfsClient.saveFile(getTableLocation(tableName, partitionName) + "/000001_0" + fileNameSuffix.orElse(""), BUCKET_1_CONTENTS);
     }
 }
