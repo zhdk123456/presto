e7612460533890dbf51a99a80bc57afef61e4eb7
Lukasz Osipiuk <lukasz@osipiuk.net>
2016-10-26 14:57:33 +0200

    Pass CostCalculator to PlanPrinter

--- presto-main/src/main/java/com/facebook/presto/testing/LocalQueryRunner.java
+++ presto-main/src/main/java/com/facebook/presto/testing/LocalQueryRunner.java
@@ -551,7 +551,7 @@ public class LocalQueryRunner
         Plan plan = createPlan(session, sql);
 
         if (printPlan) {
-            System.out.println(PlanPrinter.textLogicalPlan(plan.getRoot(), plan.getTypes(), metadata, session));
+            System.out.println(PlanPrinter.textLogicalPlan(plan.getRoot(), plan.getTypes(), metadata, costCalculator, session));
         }
 
         SubPlan subplan = PlanFragmenter.createSubPlans(session, metadata, plan);
@@ -562,6 +562,7 @@ public class LocalQueryRunner
         LocalExecutionPlanner executionPlanner = new LocalExecutionPlanner(
                 metadata,
                 sqlParser,
+                costCalculator,
                 Optional.empty(),
                 pageSourceManager,
                 indexManager,
@@ -683,6 +684,7 @@ public class LocalQueryRunner
                 metadata,
                 accessControl,
                 sqlParser,
+                costCalculator,
                 dataDefinitionTask);
         Analyzer analyzer = new Analyzer(session, metadata, sqlParser, accessControl, Optional.of(queryExplainer), parameters);
 
