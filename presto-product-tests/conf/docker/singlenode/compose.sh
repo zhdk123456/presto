3d820712214b3592e0a8646f289da1ecf6d81fcc
Grzegorz Kokosiński <Grzegorz.Kokosinski@Teradata.com>
2016-10-26 09:02:44 +0200

    Split standard compose file into: base, hive and presto

--- presto-product-tests/conf/docker/singlenode/compose.sh
+++ presto-product-tests/conf/docker/singlenode/compose.sh
@@ -3,7 +3,9 @@
 source ${BASH_SOURCE%/*}/../common/compose-commons.sh
 
 docker-compose \
--f ${BASH_SOURCE%/*}/../common/standard.yml \
+-f ${BASH_SOURCE%/*}/../common/base.yml \
+-f ${BASH_SOURCE%/*}/../common/hive.yml \
+-f ${BASH_SOURCE%/*}/../common/presto.yml \
 -f ${BASH_SOURCE%/*}/../common/jdbc_db.yml \
 -f ${BASH_SOURCE%/*}/docker-compose.yml \
 "$@"
