bfed60891260ec53b5e8ddf3cc46ed32f684b97d
Andrii Rosa <Andriy.Rosa@TERADATA.COM>
2016-10-27 13:49:41 -0400

    Pass list of projection columns to SplitManager

--- presto-main/src/main/java/com/facebook/presto/split/SplitManager.java
+++ presto-main/src/main/java/com/facebook/presto/split/SplitManager.java
@@ -16,15 +16,18 @@ package com.facebook.presto.split;
 import com.facebook.presto.Session;
 import com.facebook.presto.connector.ConnectorId;
 import com.facebook.presto.metadata.TableLayoutHandle;
+import com.facebook.presto.spi.ColumnHandle;
 import com.facebook.presto.spi.ConnectorSession;
 import com.facebook.presto.spi.ConnectorSplitSource;
 import com.facebook.presto.spi.connector.ConnectorSplitManager;
 
+import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkState;
+import static java.util.Objects.requireNonNull;
 
 public class SplitManager
 {
@@ -35,8 +38,9 @@ public class SplitManager
         checkState(splitManagers.putIfAbsent(connectorId, connectorSplitManager) == null, "SplitManager for connector '%s' is already registered", connectorId);
     }
 
-    public SplitSource getSplits(Session session, TableLayoutHandle layout)
+    public SplitSource getSplits(Session session, TableLayoutHandle layout, List<ColumnHandle> columns)
     {
+        requireNonNull(columns, "columns is null");
         ConnectorId connectorId = layout.getConnectorId();
         ConnectorSplitManager splitManager = getConnectorSplitManager(connectorId);
 
