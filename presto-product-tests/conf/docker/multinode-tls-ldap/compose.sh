0ec3a3729e9733b376130c0e1d7072c410c853cd
Andrii Rosa <Andriy.Rosa@TERADATA.COM>
2016-10-27 15:24:53 -0400

    Specify tempto configuration for tls-ldap with environment variable
    As in any other environment now

--- presto-product-tests/conf/docker/multinode-tls-ldap/compose.sh
+++ presto-product-tests/conf/docker/multinode-tls-ldap/compose.sh
@@ -4,6 +4,9 @@ SCRIPT_DIRECTORY=${BASH_SOURCE%/*}
 
 source ${SCRIPT_DIRECTORY}/../common/compose-commons.sh
 
+TEMPTO_CONFIG_YAML_DEFAULT="${PRODUCT_TESTS_ROOT}/conf/tempto/tempto-configuration-for-docker-tls-ldap.yaml"
+export TEMPTO_CONFIG_YAML=$(canonical_path ${TEMPTO_CONFIG_YAML:-${TEMPTO_CONFIG_YAML_DEFAULT}})
+
 docker-compose \
 -f ${SCRIPT_DIRECTORY}/../common/standard.yml \
 -f ${SCRIPT_DIRECTORY}/../common/jdbc_db.yml \
