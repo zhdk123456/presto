c4105473c621744677941fa375b549df0802a653
Artur Gajowy <artur.gajowy@gmail.com>
2016-11-07 10:32:21 +0100

    Q6 failing

--- presto-tpcds/src/test/java/com/facebook/presto/tpcds/TestQueryPlansDeterministic.java
+++ presto-tpcds/src/test/java/com/facebook/presto/tpcds/TestQueryPlansDeterministic.java
@@ -11,22 +11,18 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.facebook.presto.tests;
+package com.facebook.presto.tpcds;
 
 import com.facebook.presto.Session;
-import com.facebook.presto.connector.ConnectorId;
 import com.facebook.presto.metadata.SessionPropertyManager;
 import com.facebook.presto.testing.LocalQueryRunner;
 import com.facebook.presto.testing.MaterializedResult;
 import com.facebook.presto.testing.TestingAccessControlManager;
-import com.facebook.presto.tpch.TpchConnectorFactory;
+import com.facebook.presto.tests.AbstractTestQueries;
 import com.google.common.collect.ImmutableMap;
-import org.intellij.lang.annotations.Language;
 import org.testng.annotations.Test;
 
-import static com.facebook.presto.testing.TestingSession.TESTING_CATALOG;
 import static com.facebook.presto.testing.TestingSession.testSessionBuilder;
-import static com.facebook.presto.tpch.TpchMetadata.TINY_SCHEMA_NAME;
 import static org.testng.Assert.assertEquals;
 
 public class TestQueryPlansDeterministic
@@ -43,7 +39,7 @@ public class TestQueryPlansDeterministic
     {
         Session defaultSession = testSessionBuilder()
                 .setCatalog("local")
-                .setSchema(TINY_SCHEMA_NAME)
+                .setSchema("sf1")
                 .build();
 
         LocalQueryRunner localQueryRunner = new LocalQueryRunner(defaultSession);
@@ -52,14 +48,14 @@ public class TestQueryPlansDeterministic
         // local queries run directly against the generator
         localQueryRunner.createCatalog(
                 defaultSession.getCatalog().get(),
-                new TpchConnectorFactory(1),
+                new TpcdsConnectorFactory(1),
                 ImmutableMap.<String, String>of());
 
         localQueryRunner.getMetadata().addFunctions(CUSTOM_FUNCTIONS);
 
         SessionPropertyManager sessionPropertyManager = localQueryRunner.getMetadata().getSessionPropertyManager();
         sessionPropertyManager.addSystemSessionProperties(TEST_SYSTEM_PROPERTIES);
-        sessionPropertyManager.addConnectorSessionProperties(new ConnectorId(TESTING_CATALOG), TEST_CATALOG_PROPERTIES);
+        sessionPropertyManager.addConnectorSessionProperties("connector", TEST_CATALOG_PROPERTIES);
 
         return localQueryRunner;
     }
@@ -103,6 +99,35 @@ public class TestQueryPlansDeterministic
                 "  o_year DESC\n");
     }
 
+    @Test
+    public void testTpchQ6()
+            throws Exception
+    {
+        assertQuery("SELECT a.ca_state STATE,\n" +
+                "                  count(*) cnt\n" +
+                "FROM \"sf1\".customer_address a,\n" +
+                "     \"sf1\".customer c,\n" +
+                "     \"sf1\".store_sales s,\n" +
+                "     \"sf1\".date_dim d,\n" +
+                "     \"sf1\".item i\n" +
+                "WHERE a.ca_address_sk = c.c_current_addr_sk\n" +
+                "  AND c.c_customer_sk = s.ss_customer_sk\n" +
+                "  AND s.ss_sold_date_sk = d.d_date_sk\n" +
+                "  AND s.ss_item_sk = i.i_item_sk\n" +
+                "  AND d.d_month_seq =\n" +
+                "    (SELECT DISTINCT (d_month_seq)\n" +
+                "     FROM \"sf1\".date_dim\n" +
+                "     WHERE d_year = 2001 \n" +
+                "       AND d_moy = 1)\n" +
+                "  AND i.i_current_price > 1.2 *\n" +
+                "    (SELECT avg(j.i_current_price)\n" +
+                "     FROM \"sf1\".item j\n" +
+                "     WHERE j.i_category = i.i_category)\n" +
+                "GROUP BY a.ca_state\n" +
+                "HAVING count(*) >= 10\n" +
+                "ORDER BY cnt LIMIT 100\n");
+    }
+
     private void checkPlanIsDeterministic(String sql)
     {
         MaterializedResult result = computeActual("EXPLAIN " + sql);
@@ -128,126 +153,124 @@ public class TestQueryPlansDeterministic
     }
 
     @Override
-    protected void assertQuery(@Language("SQL") String sql)
+    protected void assertQuery(String sql)
             throws Exception
     {
         checkPlanIsDeterministic(sql);
     }
 
     @Override
-    protected void assertQuery(Session session, @Language("SQL") String sql)
+    protected void assertQuery(Session session, String sql)
             throws Exception
     {
         checkPlanIsDeterministic(sql);
     }
 
     @Override
-    public void assertQueryOrdered(@Language("SQL") String sql)
+    public void assertQueryOrdered(String sql)
             throws Exception
     {
         checkPlanIsDeterministic(sql);
     }
 
     @Override
-    protected void assertQuery(@Language("SQL") String actual, @Language("SQL") String expected)
+    protected void assertQuery(String actual, String expected)
             throws Exception
     {
         checkPlanIsDeterministic(actual);
     }
 
     @Override
-    protected void assertQuery(Session session, @Language("SQL") String actual, @Language("SQL") String expected)
+    protected void assertQuery(Session session, String actual, String expected)
             throws Exception
     {
         checkPlanIsDeterministic(actual);
     }
 
     @Override
-    protected void assertQueryOrdered(@Language("SQL") String actual, @Language("SQL") String expected)
+    protected void assertQueryOrdered(String actual, String expected)
             throws Exception
     {
         checkPlanIsDeterministic(actual);
     }
 
     @Override
-    protected void assertQueryOrdered(Session session, @Language("SQL") String actual, @Language("SQL") String expected)
+    protected void assertQueryOrdered(Session session, String actual, String expected)
             throws Exception
     {
         checkPlanIsDeterministic(actual);
     }
 
     @Override
-    protected void assertUpdate(@Language("SQL") String actual, @Language("SQL") String expected)
+    protected void assertUpdate(String actual, String expected)
             throws Exception
     {
         checkPlanIsDeterministic(actual);
     }
 
     @Override
-    protected void assertUpdate(Session session, @Language("SQL") String actual, @Language("SQL") String expected)
+    protected void assertUpdate(Session session, String actual, String expected)
             throws Exception
     {
         checkPlanIsDeterministic(actual);
     }
 
     @Override
-    protected void assertUpdate(@Language("SQL") String sql)
+    protected void assertUpdate(String sql)
     {
         checkPlanIsDeterministic(sql);
     }
 
     @Override
-    protected void assertUpdate(Session session, @Language("SQL") String sql)
+    protected void assertUpdate(Session session, String sql)
     {
         checkPlanIsDeterministic(sql);
     }
 
     @Override
-    protected void assertUpdate(@Language("SQL") String sql, long count)
+    protected void assertUpdate(String sql, long count)
     {
         checkPlanIsDeterministic(sql);
     }
 
     @Override
-    protected void assertUpdate(Session session, @Language("SQL") String sql, long count)
+    protected void assertUpdate(Session session, String sql, long count)
     {
         checkPlanIsDeterministic(sql);
     }
 
     @Override
-    protected void assertQueryFails(@Language("SQL") String sql, @Language("RegExp") String expectedMessageRegExp)
+    protected void assertQueryFails(String sql, String expectedMessageRegExp)
     {
     }
 
     @Override
-    protected void assertQueryFails(Session session, @Language("SQL") String sql, @Language("RegExp") String expectedMessageRegExp)
+    protected void assertQueryFails(Session session, String sql, String expectedMessageRegExp)
     {
     }
 
     @Override
-    protected void assertAccessAllowed(@Language("SQL") String sql, TestingAccessControlManager.TestingPrivilege... deniedPrivileges)
+    protected void assertAccessAllowed(String sql, TestingAccessControlManager.TestingPrivilege... deniedPrivileges)
             throws Exception
     {
     }
 
     @Override
-    protected void assertAccessAllowed(Session session, @Language("SQL") String sql, TestingAccessControlManager.TestingPrivilege... deniedPrivileges)
-            throws Exception
-    {
-
-    }
-
-    @Override
-    protected void assertAccessDenied(@Language("SQL") String sql, @Language("RegExp") String exceptionsMessageRegExp, TestingAccessControlManager.TestingPrivilege... deniedPrivileges)
+    protected void assertAccessAllowed(Session session, String sql, TestingAccessControlManager.TestingPrivilege... deniedPrivileges)
             throws Exception
     {
     }
 
     @Override
-    protected void assertAccessDenied(Session session, @Language("SQL") String sql, @Language("RegExp") String exceptionsMessageRegExp, TestingAccessControlManager.TestingPrivilege... deniedPrivileges)
+    protected void assertAccessDenied(String sql, String exceptionsMessageRegExp, TestingAccessControlManager.TestingPrivilege... deniedPrivileges)
             throws Exception
     {
+    }
 
+    @Override
+    protected void assertAccessDenied(Session session, String sql, String exceptionsMessageRegExp, TestingAccessControlManager.TestingPrivilege... deniedPrivileges)
+            throws Exception
+    {
     }
 
     @Override
