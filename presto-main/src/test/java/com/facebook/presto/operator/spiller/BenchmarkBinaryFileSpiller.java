e5448591268be918f6148f74090688e00b9c8976
Lukasz Osipiuk <lukasz@osipiuk.net>
2016-11-10 15:20:08 +0100

    Replace BinaryFileSpiller with parametrization of GenericSpiller

--- presto-main/src/test/java/com/facebook/presto/operator/spiller/BenchmarkBinaryFileSpiller.java
+++ presto-main/src/test/java/com/facebook/presto/operator/spiller/BenchmarkBinaryFileSpiller.java
@@ -18,9 +18,11 @@ import com.facebook.presto.spi.Page;
 import com.facebook.presto.spi.PageBuilder;
 import com.facebook.presto.spi.block.BlockEncodingSerde;
 import com.facebook.presto.spi.type.Type;
-import com.facebook.presto.spiller.BinarySpillerFactory;
+import com.facebook.presto.spiller.BinaryFileSingleStreamSpillerFactory;
+import com.facebook.presto.spiller.GenericSpillerFactory;
 import com.facebook.presto.spiller.LocalSpillContext;
 import com.facebook.presto.spiller.Spiller;
+import com.facebook.presto.spiller.SpillerFactory;
 import com.facebook.presto.spiller.SpillerStats;
 import com.facebook.presto.type.TypeRegistry;
 import com.google.common.collect.ImmutableList;
@@ -90,7 +92,8 @@ public class BenchmarkBinaryFileSpiller
     {
         private final ListeningExecutorService executor = MoreExecutors.newDirectExecutorService();
         private final SpillerStats spillerStats = new SpillerStats();
-        private final BinarySpillerFactory spillerFactory = new BinarySpillerFactory(executor, BLOCK_ENCODING_MANAGER, spillerStats, SPILL_PATH);
+        private final SpillerFactory spillerFactory = new GenericSpillerFactory(
+                new BinaryFileSingleStreamSpillerFactory(executor, BLOCK_ENCODING_MANAGER, spillerStats, ImmutableList.of(SPILL_PATH), 1.0));
 
         @Param({"10000"})
         private int rowsPerPage = 10000;
