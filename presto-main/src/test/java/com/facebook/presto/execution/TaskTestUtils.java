abbd5084db9e2c76d8f4bd01e063c54627819585
Lukasz Osipiuk <lukasz@osipiuk.net>
2016-11-10 15:20:08 +0100

    Replace BinaryFileSpiller with parametrization of GenericSpiller

--- presto-main/src/test/java/com/facebook/presto/execution/TaskTestUtils.java
+++ presto-main/src/test/java/com/facebook/presto/execution/TaskTestUtils.java
@@ -32,7 +32,8 @@ import com.facebook.presto.operator.index.IndexJoinLookupStats;
 import com.facebook.presto.spi.connector.ConnectorTransactionHandle;
 import com.facebook.presto.spi.predicate.TupleDomain;
 import com.facebook.presto.spi.type.Type;
-import com.facebook.presto.spiller.BinarySpillerFactory;
+import com.facebook.presto.spiller.BinaryFileSingleStreamSpillerFactory;
+import com.facebook.presto.spiller.GenericSpillerFactory;
 import com.facebook.presto.spiller.SpillerStats;
 import com.facebook.presto.split.PageSinkManager;
 import com.facebook.presto.split.PageSourceManager;
@@ -132,7 +133,7 @@ public final class TaskTestUtils
                 new IndexJoinLookupStats(),
                 new CompilerConfig(),
                 new TaskManagerConfig(),
-                new BinarySpillerFactory(new BlockEncodingManager(metadata.getTypeManager()), new SpillerStats(), new FeaturesConfig()));
+                new GenericSpillerFactory(new BinaryFileSingleStreamSpillerFactory(new BlockEncodingManager(metadata.getTypeManager()), new SpillerStats(), new FeaturesConfig())));
     }
 
     public static TaskInfo updateTask(SqlTask sqlTask, List<TaskSource> taskSources, OutputBuffers outputBuffers)
