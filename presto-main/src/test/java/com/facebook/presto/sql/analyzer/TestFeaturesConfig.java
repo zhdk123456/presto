700b189a41cf5b5b939d43a8007e5dbfdf199f11
Lukasz Osipiuk <lukasz@osipiuk.net>
2016-11-10 14:47:22 +0100

    Introduce BinaryFileSingleStreamSpillerFactory

--- presto-main/src/test/java/com/facebook/presto/sql/analyzer/TestFeaturesConfig.java
+++ presto-main/src/test/java/com/facebook/presto/sql/analyzer/TestFeaturesConfig.java
@@ -54,8 +54,9 @@ public class TestFeaturesConfig
                 .setRe2JDfaRetries(5)
                 .setSpillEnabled(false)
                 .setOperatorMemoryLimitBeforeSpill(DataSize.valueOf("4MB"))
-                .setSpillerSpillPath(Paths.get(System.getProperty("java.io.tmpdir"), "presto", "spills").toString())
+                .setSpillerSpillPaths(Paths.get(System.getProperty("java.io.tmpdir"), "presto", "spills").toString())
                 .setSpillerThreads(4)
+                .setSpillMaxUsedSpaceThreshold(0.9)
                 .setOptimizeMixedDistinctAggregations(false)
                 .setLegacyOrderBy(false));
     }
@@ -85,8 +86,9 @@ public class TestFeaturesConfig
                 .put("re2j.dfa-retries", "42")
                 .put("experimental.spill-enabled", "true")
                 .put("experimental.operator-memory-limit-before-spill", "100MB")
-                .put("experimental.spiller-spill-path", "/tmp/custom/spill/path")
+                .put("experimental.spiller-spill-path", "/tmp/custom/spill/path1,/tmp/custom/spill/path2")
                 .put("experimental.spiller-threads", "42")
+                .put("experimental.spiller-max-used-space-threshold", "0.8")
                 .build();
         Map<String, String> properties = new ImmutableMap.Builder<String, String>()
                 .put("experimental.resource-groups-enabled", "true")
@@ -110,8 +112,9 @@ public class TestFeaturesConfig
                 .put("re2j.dfa-retries", "42")
                 .put("experimental.spill-enabled", "true")
                 .put("experimental.operator-memory-limit-before-spill", "100MB")
-                .put("experimental.spiller-spill-path", "/tmp/custom/spill/path")
+                .put("experimental.spiller-spill-path", "/tmp/custom/spill/path1,/tmp/custom/spill/path2")
                 .put("experimental.spiller-threads", "42")
+                .put("experimental.spiller-max-used-space-threshold", "0.8")
                 .build();
 
         FeaturesConfig expected = new FeaturesConfig()
@@ -135,8 +138,9 @@ public class TestFeaturesConfig
                 .setRe2JDfaRetries(42)
                 .setSpillEnabled(true)
                 .setOperatorMemoryLimitBeforeSpill(DataSize.valueOf("100MB"))
-                .setSpillerSpillPath("/tmp/custom/spill/path")
+                .setSpillerSpillPaths("/tmp/custom/spill/path1,/tmp/custom/spill/path2")
                 .setSpillerThreads(42)
+                .setSpillMaxUsedSpaceThreshold(0.8)
                 .setLegacyOrderBy(true);
 
         assertFullMapping(properties, expected);
