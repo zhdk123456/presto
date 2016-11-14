940bf592f98c85388ef96cf1c46479daf628c0c1
Piotr Nowojski <piotr.nowojski@teradata.com>
2016-11-14 08:32:33 +0100

    Pass types to CostCalculator

--- presto-main/src/main/java/com/facebook/presto/sql/planner/PlanPrinter.java
+++ presto-main/src/main/java/com/facebook/presto/sql/planner/PlanPrinter.java
@@ -158,7 +158,7 @@ public class PlanPrinter
         this.metadata = metadata;
         this.stats = Optional.empty();
 
-        Map<PlanNode, PlanNodeCost> costs = costCalculator.calculateCostForPlan(session, plan);
+        Map<PlanNode, PlanNodeCost> costs = costCalculator.calculateCostForPlan(session, types, plan);
         Visitor visitor = new Visitor(types, costs, session);
         plan.accept(visitor, indent);
     }
@@ -173,7 +173,7 @@ public class PlanPrinter
         this.metadata = metadata;
         this.stats = Optional.of(stats);
 
-        Map<PlanNode, PlanNodeCost> costs = costCalculator.calculateCostForPlan(session, plan);
+        Map<PlanNode, PlanNodeCost> costs = costCalculator.calculateCostForPlan(session, types, plan);
         Visitor visitor = new Visitor(types, costs, session);
         plan.accept(visitor, indent);
     }
