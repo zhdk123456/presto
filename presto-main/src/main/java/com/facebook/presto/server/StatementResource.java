fb131de1c259a271889cefb33ca983a929aed57c
Wojciech Biela <Wojciech.Biela@teradata.com>
2016-11-17 10:11:16 +0100

    add spilled data size to query summary debug
    When CLI is started with --debug then spilled data size for the entire
    query is being displayed (both in the running total, and the final summary).
    The information is added in a separate line below "Parallelism".

--- presto-main/src/main/java/com/facebook/presto/server/StatementResource.java
+++ presto-main/src/main/java/com/facebook/presto/server/StatementResource.java
@@ -615,6 +615,7 @@ public class StatementResource
                     .setProcessedRows(queryStats.getRawInputPositions())
                     .setProcessedBytes(queryStats.getRawInputDataSize().toBytes())
                     .setRootStage(toStageStats(outputStage))
+                    .setSpilledDataSize(queryStats.getSpilledDataSize())
                     .build();
         }
 
