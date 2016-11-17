cdccdbe1199108cd3a10d6bd62f763719df5ce0c
Rebecca Schlussel <rebecca.schlussel@teradata.com>
2016-11-17 09:30:23 -0500

    Remove distributed join property

--- presto-main/src/main/java/com/facebook/presto/sql/analyzer/FeaturesConfig.java
+++ presto-main/src/main/java/com/facebook/presto/sql/analyzer/FeaturesConfig.java
@@ -55,7 +55,6 @@ public class FeaturesConfig
     }
 
     private boolean distributedIndexJoinsEnabled;
-    private boolean distributedJoinsEnabled = true;
     private boolean colocatedJoinsEnabled;
     private boolean reorderJoins;
     private boolean redistributeWrites = true;
@@ -113,11 +112,6 @@ public class FeaturesConfig
         return this;
     }
 
-    public boolean isDistributedJoinsEnabled()
-    {
-        return distributedJoinsEnabled;
-    }
-
     @Config("deprecated.legacy-array-agg")
     public FeaturesConfig setLegacyArrayAgg(boolean legacyArrayAgg)
     {
@@ -152,13 +146,6 @@ public class FeaturesConfig
     public boolean isLegacyMapSubscript()
     {
         return legacyMapSubscript;
-    }
-
-    @Config("distributed-joins-enabled")
-    public FeaturesConfig setDistributedJoinsEnabled(boolean distributedJoinsEnabled)
-    {
-        this.distributedJoinsEnabled = distributedJoinsEnabled;
-        return this;
     }
 
     public boolean isColocatedJoinsEnabled()
