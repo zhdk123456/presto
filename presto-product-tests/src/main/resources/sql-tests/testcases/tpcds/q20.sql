22513a50cfa45edd9ab5f2d5ae0a287c44ee019f
Grzegorz Kokosiński <Grzegorz.Kokosinski@Teradata.com>
2016-11-17 14:12:30 +0100

    Add TPC-DS queries as product tests
    tpcds tests are disabled to be run in each travis build. It is going to
    be run externally in prest-checks. This is due that execution duration
    in about 2 hours.
    
    Result files for quarantined could be not formatted properly to be
    accepted by tempto. It is because, typically result files are generated
    by tempto, but for queries which returns incorrect results result files
    comes from TPC-DS benchmark.

---
+++ presto-product-tests/src/main/resources/sql-tests/testcases/tpcds/q20.sql
@@ -0,0 +1,26 @@
+-- database: presto_tpcds; groups: tpcds; requires: com.teradata.tempto.fulfillment.table.hive.tpcds.ImmutableTpcdsTablesRequirements
+SELECT i_item_id,
+       i_item_desc,
+       i_category,
+       i_class,
+       i_current_price,
+       sum(cs_ext_sales_price) AS itemrevenue,
+       sum(cs_ext_sales_price)*100/sum(sum(cs_ext_sales_price)) over (partition BY i_class) AS revenueratio
+FROM catalog_sales,
+     item,
+     date_dim
+WHERE cs_item_sk = i_item_sk
+  AND i_category IN ('Sports',
+                     'Books',
+                     'Home') AND cs_sold_date_sk = d_date_sk
+  AND cast(d_date as date) BETWEEN cast('1999-02-22' AS date) AND (cast('1999-02-22' AS date) + INTERVAL '30' DAY)
+GROUP BY i_item_id,
+         i_item_desc,
+         i_category,
+         i_class,
+         i_current_price
+ORDER BY i_category,
+         i_class,
+         i_item_id,
+         i_item_desc,
+         revenueratio LIMIT 100;
---
+++ presto-product-tests/src/main/resources/sql-tests/testcases/tpcds/q32.sql
@@ -0,0 +1,16 @@
+-- database: presto_tpcds; groups: tpcds; requires: com.teradata.tempto.fulfillment.table.hive.tpcds.ImmutableTpcdsTablesRequirements
+SELECT sum(cs_ext_discount_amt) AS "excess discount amount"
+FROM catalog_sales,
+     item,
+     date_dim
+WHERE i_manufact_id = 977 
+  AND i_item_sk = cs_item_sk
+  AND d_date BETWEEN cast('2000-01-27' AS DATE) AND (cast('2000-01-27' AS DATE) + INTERVAL '90' DAY)
+  AND d_date_sk = cs_sold_date_sk
+  AND cs_ext_discount_amt >
+    (SELECT 1.3 * avg(cs_ext_discount_amt)
+     FROM catalog_sales,
+          date_dim
+     WHERE cs_item_sk = i_item_sk
+       AND d_date BETWEEN cast('2000-01-27' AS DATE) AND (cast('2000-01-27' AS DATE) + INTERVAL '90' DAY)
+       AND d_date_sk = cs_sold_date_sk) LIMIT 100;
