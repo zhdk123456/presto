99de6c4d41c2b55a58d7b7716cf1e9fa94e34064
Lukasz Osipiuk <lukasz@osipiuk.net>
2016-11-17 13:10:30 +0100

    Pass supplier of LocalSpillContext to spiller

--- presto-main/src/test/java/com/facebook/presto/spiller/TestBinaryFileSpiller.java
+++ presto-main/src/test/java/com/facebook/presto/spiller/TestBinaryFileSpiller.java
@@ -51,7 +51,7 @@ public class TestBinaryFileSpiller
     public void testFileSpiller()
             throws Exception
     {
-        try (Spiller spiller = factory.create(TYPES, new LocalSpillContext(new BenchmarkBinaryFileSpiller.TestOperatorSpillContext()))) {
+        try (Spiller spiller = factory.create(TYPES, () -> new LocalSpillContext(new BenchmarkBinaryFileSpiller.TestOperatorSpillContext()))) {
             testSimpleSpiller(spiller);
         }
     }
@@ -72,7 +72,7 @@ public class TestBinaryFileSpiller
 
         Page page = new Page(col1.build(), col2.build(), col3.build());
 
-        try (Spiller spiller = factory.create(TYPES,  new LocalSpillContext(new BenchmarkBinaryFileSpiller.TestOperatorSpillContext()))) {
+        try (Spiller spiller = factory.create(TYPES,  () -> new LocalSpillContext(new BenchmarkBinaryFileSpiller.TestOperatorSpillContext()))) {
             testSpiller(types, spiller, ImmutableList.of(page));
         }
     }
