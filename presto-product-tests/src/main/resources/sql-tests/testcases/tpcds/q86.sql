2cf76b61bdb343c0ab75b478980077bcf6f0cd22
Grzegorz Kokosiński <Grzegorz.Kokosinski@Teradata.com>
2016-11-17 14:12:30 +0100

    Add TPC-DS queries as product tests
    tpcds tests are disabled to be run in each travis build. It is going to
    be run externally in prest-checks. This is due that execution duration
    in about 2 hours.
    
    Result files for quarantined could be not formatted properly to be
    accepted by tempto. It is because, typically result files are generated
    by tempto, but for queries which returns incorrect results result files
    comes from TPC-DS benchmark.

---
+++ presto-product-tests/src/main/resources/sql-tests/testcases/tpcds/q86.sql
@@ -0,0 +1,20 @@
+-- database: presto_tpcds; groups: tpcds, quarantine; requires: com.teradata.tempto.fulfillment.table.hive.tpcds.ImmutableTpcdsTablesRequirements
+--- fails with:  line 4:8: no viable alternative at input 'grouping'
+SELECT sum(ws_net_paid) AS total_sum,
+       i_category,
+       i_class,
+       grouping(i_category)+grouping(i_class) AS lochierarchy,
+       rank() over ( partition BY grouping(i_category)+grouping(i_class), CASE WHEN grouping(i_class) = 0 THEN i_category END
+                    ORDER BY sum(ws_net_paid) DESC) AS rank_within_parent
+FROM web_sales,
+     date_dim d1,
+     item
+WHERE d1.d_month_seq BETWEEN 1200 AND 1200+11
+  AND d1.d_date_sk = ws_sold_date_sk
+  AND i_item_sk = ws_item_sk
+GROUP BY rollup(i_category,i_class)
+ORDER BY lochierarchy DESC,
+         CASE
+             WHEN lochierarchy = 0 THEN i_category
+         END,
+         rank_within_parent LIMIT 100;
