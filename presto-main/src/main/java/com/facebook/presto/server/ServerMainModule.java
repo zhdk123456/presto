00ec3c2c4bb5e2cf82a2dd2983153bec1895a9ea
Andrii Rosa <Andriy.Rosa@TERADATA.COM>
2016-11-21 12:10:57 -0500

    Support secure internal communication

--- presto-main/src/main/java/com/facebook/presto/server/ServerMainModule.java
+++ presto-main/src/main/java/com/facebook/presto/server/ServerMainModule.java
@@ -117,6 +117,7 @@ import com.google.inject.TypeLiteral;
 import io.airlift.concurrent.BoundedExecutor;
 import io.airlift.configuration.AbstractConfigurationAwareModule;
 import io.airlift.discovery.client.ServiceDescriptor;
+import io.airlift.http.client.HttpClientConfig;
 import io.airlift.node.NodeInfo;
 import io.airlift.slice.Slice;
 import io.airlift.stats.PauseMeter;
@@ -179,6 +180,12 @@ public class ServerMainModule
                 throw new UnsupportedOperationException();
             }));
         }
+
+        InternalCommunicationConfig internalCommunicationConfig = buildConfigObject(InternalCommunicationConfig.class);
+        configBinder(binder).bindConfigGlobalDefaults(HttpClientConfig.class, config -> {
+            config.setKeyStorePath(internalCommunicationConfig.getKeyStorePath());
+            config.setKeyStorePassword(internalCommunicationConfig.getKeyStorePassword());
+        });
 
         configBinder(binder).bindConfig(FeaturesConfig.class);
 
