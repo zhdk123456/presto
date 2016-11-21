9d617e6a9cc9e2fcec75b72dc3054e0228ea6dd9
Anton Petrov <petroav@users.noreply.github.com>
2016-11-21 16:01:08 -0500

    Keep track of groupId symbols per QuerySpecification

--- presto-main/src/main/java/com/facebook/presto/sql/analyzer/Analysis.java
+++ presto-main/src/main/java/com/facebook/presto/sql/analyzer/Analysis.java
@@ -18,6 +18,7 @@ import com.facebook.presto.metadata.Signature;
 import com.facebook.presto.metadata.TableHandle;
 import com.facebook.presto.spi.ColumnHandle;
 import com.facebook.presto.spi.type.Type;
+import com.facebook.presto.sql.planner.Symbol;
 import com.facebook.presto.sql.tree.DefaultTraversalVisitor;
 import com.facebook.presto.sql.tree.ExistsPredicate;
 import com.facebook.presto.sql.tree.Expression;
@@ -91,6 +92,8 @@ public class Analysis
     private final IdentityMap<Field, ColumnHandle> columns = new IdentityMap<>(LinkedHashMap::new);
 
     private final IdentityMap<SampledRelation, Double> sampleRatios = new IdentityMap<>(LinkedHashMap::new);
+
+    private final IdentityMap<QuerySpecification, Symbol> groupIdSymbols = new IdentityMap<>(LinkedHashMap::new);
 
     // for create table
     private Optional<QualifiedObjectName> createTableDestination = Optional.empty();
@@ -522,6 +525,16 @@ public class Analysis
     public boolean isDescribe()
     {
         return isDescribe;
+    }
+
+    public void addGroupIdSymbol(QuerySpecification node, Symbol groupIdSymbol)
+    {
+        this.groupIdSymbols.put(node, groupIdSymbol);
+    }
+
+    public Optional<Symbol> getGroupIdSymbol(QuerySpecification querySpecification)
+    {
+        return Optional.ofNullable(this.groupIdSymbols.get(querySpecification));
     }
 
     @Immutable
