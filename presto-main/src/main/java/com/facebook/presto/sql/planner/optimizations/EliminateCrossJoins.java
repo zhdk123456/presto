5e6b2ff6a28f2b1a0541b46723a6c81a3237916e
Karol Sobczak <Karol.Sobczak@Teradata.com>
2016-11-21 08:42:23 +0100

    Adjust cross join elimination code to join pruning

--- presto-main/src/main/java/com/facebook/presto/sql/planner/optimizations/EliminateCrossJoins.java
+++ presto-main/src/main/java/com/facebook/presto/sql/planner/optimizations/EliminateCrossJoins.java
@@ -180,6 +180,10 @@ public class EliminateCrossJoins
                         result,
                         rightNode,
                         criteria.build(),
+                        ImmutableList.<Symbol>builder()
+                                .addAll(result.getOutputSymbols())
+                                .addAll(rightNode.getOutputSymbols())
+                                .build(),
                         Optional.empty(),
                         Optional.empty(),
                         Optional.empty());
