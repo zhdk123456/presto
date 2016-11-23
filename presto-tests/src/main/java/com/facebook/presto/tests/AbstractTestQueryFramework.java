f1eed6ea1a7f5ce154f478095599eb2153ece41e
Andrzej Fiedukowicz <Andrzej.Fiedukowicz@teradata.com>
2016-11-23 12:09:52 +0100

    Revert "Log tests executed by AbstractTestQueryFramework"
    This reverts commit 44c15ffca2c880589aa29b04349c696a8bbf5ec7.

--- presto-tests/src/main/java/com/facebook/presto/tests/AbstractTestQueryFramework.java
+++ presto-tests/src/main/java/com/facebook/presto/tests/AbstractTestQueryFramework.java
@@ -33,7 +33,6 @@ import com.google.common.collect.ImmutableMap;
 import org.intellij.lang.annotations.Language;
 import org.testng.SkipException;
 import org.testng.annotations.AfterClass;
-import org.testng.annotations.Listeners;
 
 import java.util.List;
 import java.util.Optional;
@@ -47,7 +46,6 @@ import static java.util.Collections.emptyList;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.fail;
 
-@Listeners({TestLoggingListener.class})
 public abstract class AbstractTestQueryFramework
 {
     protected final H2QueryRunner h2QueryRunner;
