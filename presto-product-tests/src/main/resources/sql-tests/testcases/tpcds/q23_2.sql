c69aaef4e1e9bcb8ece27e664374bc47bbe1faf2
Grzegorz Kokosiński <Grzegorz.Kokosinski@Teradata.com>
2016-11-28 15:08:09 +0100

    Remove not needed modificaiton for TPCDS q2*

--- presto-product-tests/src/main/resources/sql-tests/testcases/tpcds/q23_2.sql
+++ presto-product-tests/src/main/resources/sql-tests/testcases/tpcds/q23_2.sql
@@ -31,7 +31,7 @@ WITH
          AND ("ss_sold_date_sk" = "d_date_sk")
          AND ("d_year" IN (2000      , (2000 + 1)      , (2000 + 2)      , (2000 + 3)))
       GROUP BY "c_customer_sk"
-   )  x
+   ) 
 ) 
 , best_ss_customer AS (
    SELECT
@@ -54,54 +54,52 @@ SELECT
 , "sales"
 FROM
   (
-(
-      SELECT
-        "c_last_name"
-      , "c_first_name"
-      , "sum"(("cs_quantity" * "cs_list_price")) "sales"
+   SELECT
+     "c_last_name"
+   , "c_first_name"
+   , "sum"(("cs_quantity" * "cs_list_price")) "sales"
+   FROM
+     catalog_sales
+   , customer
+   , date_dim
+   WHERE ("d_year" = 2000)
+      AND ("d_moy" = 2)
+      AND ("cs_sold_date_sk" = "d_date_sk")
+      AND ("cs_item_sk" IN (
+      SELECT "item_sk"
       FROM
-        catalog_sales
-      , customer
-      , date_dim
-      WHERE ("d_year" = 2000)
-         AND ("d_moy" = 2)
-         AND ("cs_sold_date_sk" = "d_date_sk")
-         AND ("cs_item_sk" IN (
-         SELECT "item_sk"
-         FROM
-           frequent_ss_items
-      ))
-         AND ("cs_bill_customer_sk" IN (
-         SELECT "c_customer_sk"
-         FROM
-           best_ss_customer
-      ))
-         AND ("cs_bill_customer_sk" = "c_customer_sk")
-      GROUP BY "c_last_name", "c_first_name"
-   ) UNION ALL (
-      SELECT
-        "c_last_name"
-      , "c_first_name"
-      , "sum"(("ws_quantity" * "ws_list_price")) "sales"
+        frequent_ss_items
+   ))
+      AND ("cs_bill_customer_sk" IN (
+      SELECT "c_customer_sk"
       FROM
-        web_sales
-      , customer
-      , date_dim
-      WHERE ("d_year" = 2000)
-         AND ("d_moy" = 2)
-         AND ("ws_sold_date_sk" = "d_date_sk")
-         AND ("ws_item_sk" IN (
-         SELECT "item_sk"
-         FROM
-           frequent_ss_items
-      ))
-         AND ("ws_bill_customer_sk" IN (
-         SELECT "c_customer_sk"
-         FROM
-           best_ss_customer
-      ))
-         AND ("ws_bill_customer_sk" = "c_customer_sk")
-      GROUP BY "c_last_name", "c_first_name"
-   ) )  y
+        best_ss_customer
+   ))
+      AND ("cs_bill_customer_sk" = "c_customer_sk")
+   GROUP BY "c_last_name", "c_first_name"
+UNION ALL    SELECT
+     "c_last_name"
+   , "c_first_name"
+   , "sum"(("ws_quantity" * "ws_list_price")) "sales"
+   FROM
+     web_sales
+   , customer
+   , date_dim
+   WHERE ("d_year" = 2000)
+      AND ("d_moy" = 2)
+      AND ("ws_sold_date_sk" = "d_date_sk")
+      AND ("ws_item_sk" IN (
+      SELECT "item_sk"
+      FROM
+        frequent_ss_items
+   ))
+      AND ("ws_bill_customer_sk" IN (
+      SELECT "c_customer_sk"
+      FROM
+        best_ss_customer
+   ))
+      AND ("ws_bill_customer_sk" = "c_customer_sk")
+   GROUP BY "c_last_name", "c_first_name"
+) 
 ORDER BY "c_last_name" ASC, "c_first_name" ASC, "sales" ASC
 LIMIT 100
