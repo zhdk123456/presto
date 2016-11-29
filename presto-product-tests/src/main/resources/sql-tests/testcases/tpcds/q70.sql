8c6061107af58c086a9a1cb1849786e3d62b964c
Grzegorz Kokosiński <Grzegorz.Kokosinski@Teradata.com>
2016-11-29 10:05:42 +0100

    Format TPC-DS queries with grouping

--- presto-product-tests/src/main/resources/sql-tests/testcases/tpcds/q70.sql
+++ presto-product-tests/src/main/resources/sql-tests/testcases/tpcds/q70.sql
@@ -1,34 +1,36 @@
--- database: presto_tpcds; groups: tpcds, quarantine; requires: com.teradata.tempto.fulfillment.table.hive.tpcds.ImmutableTpcdsTablesRequirements
---- fails with line 4:8: no via ble alternative at input 'grouping'
-SELECT sum(ss_net_profit) AS total_sum,
-       s_state,
-       s_county,
-       grouping(s_state)+grouping(s_county) AS lochierarchy,
-       rank() over (partition BY grouping(s_state)+grouping(s_county), CASE WHEN grouping(s_county) = 0 THEN s_state END
-                    ORDER BY sum(ss_net_profit) DESC) AS rank_within_parent
-FROM store_sales,
-     date_dim d1,
-     store
-WHERE d1.d_month_seq BETWEEN 1200 AND 1200+11
-  AND d1.d_date_sk = ss_sold_date_sk
-  AND s_store_sk = ss_store_sk
-  AND s_state IN
-    (SELECT s_state
-     FROM
-       (SELECT s_state AS s_state,
-                          rank() over (partition BY s_state
-                                       ORDER BY sum(ss_net_profit) DESC) AS ranking
-        FROM store_sales,
-             store,
-             date_dim
-        WHERE d_month_seq BETWEEN 1200 AND 1200+11
-          AND d_date_sk = ss_sold_date_sk
-          AND s_store_sk = ss_store_sk
-        GROUP BY s_state) tmp1
-     WHERE ranking <= 5)
-GROUP BY rollup(s_state,s_county)
-ORDER BY lochierarchy DESC,
-         CASE
-             WHEN lochierarchy = 0 THEN s_state
-         END,
-         rank_within_parent LIMIT 100;
+-- database: presto_tpcds; groups: tpcds; requires: com.teradata.tempto.fulfillment.table.hive.tpcds.ImmutableTpcdsTablesRequirements
+SELECT
+  "sum"("ss_net_profit") "total_sum"
+, "s_state"
+, "s_county"
+, (GROUPING ("s_state") + GROUPING ("s_county")) "lochierarchy"
+, "rank"() OVER (PARTITION BY (GROUPING ("s_state") + GROUPING ("s_county")), (CASE WHEN (GROUPING ("s_county") = 0) THEN "s_state" END) ORDER BY "sum"("ss_net_profit") DESC) "rank_within_parent"
+FROM
+  store_sales
+, date_dim d1
+, store
+WHERE ("d1"."d_month_seq" BETWEEN 1200 AND (1200 + 11))
+   AND ("d1"."d_date_sk" = "ss_sold_date_sk")
+   AND ("s_store_sk" = "ss_store_sk")
+   AND ("s_state" IN (
+   SELECT "s_state"
+   FROM
+     (
+      SELECT
+        "s_state" "s_state"
+      , "rank"() OVER (PARTITION BY "s_state" ORDER BY "sum"("ss_net_profit") DESC) "ranking"
+      FROM
+        store_sales
+      , store
+      , date_dim
+      WHERE ("d_month_seq" BETWEEN 1200 AND (1200 + 11))
+         AND ("d_date_sk" = "ss_sold_date_sk")
+         AND ("s_store_sk" = "ss_store_sk")
+      GROUP BY "s_state"
+   )  tmp1
+   WHERE ("ranking" <= 5)
+))
+GROUP BY ROLLUP (s_state, s_county)
+ORDER BY "lochierarchy" DESC, (CASE WHEN ("lochierarchy" = 0) THEN "s_state" END) ASC, "rank_within_parent" ASC
+LIMIT 100
+;
--- presto-product-tests/src/main/resources/sql-tests/testcases/tpcds/q86.sql
+++ presto-product-tests/src/main/resources/sql-tests/testcases/tpcds/q86.sql
@@ -1,20 +1,18 @@
 -- database: presto_tpcds; groups: tpcds, quarantine; requires: com.teradata.tempto.fulfillment.table.hive.tpcds.ImmutableTpcdsTablesRequirements
---- fails with:  line 4:8: no viable alternative at input 'grouping'
-SELECT sum(ws_net_paid) AS total_sum,
-       i_category,
-       i_class,
-       grouping(i_category)+grouping(i_class) AS lochierarchy,
-       rank() over ( partition BY grouping(i_category)+grouping(i_class), CASE WHEN grouping(i_class) = 0 THEN i_category END
-                    ORDER BY sum(ws_net_paid) DESC) AS rank_within_parent
-FROM web_sales,
-     date_dim d1,
-     item
-WHERE d1.d_month_seq BETWEEN 1200 AND 1200+11
-  AND d1.d_date_sk = ws_sold_date_sk
-  AND i_item_sk = ws_item_sk
-GROUP BY rollup(i_category,i_class)
-ORDER BY lochierarchy DESC,
-         CASE
-             WHEN lochierarchy = 0 THEN i_category
-         END,
-         rank_within_parent LIMIT 100;
+SELECT
+  "sum"("ws_net_paid") "total_sum"
+, "i_category"
+, "i_class"
+, (GROUPING ("i_category") + GROUPING ("i_class")) "lochierarchy"
+, "rank"() OVER (PARTITION BY (GROUPING ("i_category") + GROUPING ("i_class")), (CASE WHEN (GROUPING ("i_class") = 0) THEN "i_category" END) ORDER BY "sum"("ws_net_paid") DESC) "rank_within_parent"
+FROM
+  web_sales
+, date_dim d1
+, item
+WHERE ("d1"."d_month_seq" BETWEEN 1200 AND (1200 + 11))
+   AND ("d1"."d_date_sk" = "ws_sold_date_sk")
+   AND ("i_item_sk" = "ws_item_sk")
+GROUP BY ROLLUP (i_category, i_class)
+ORDER BY "lochierarchy" DESC, (CASE WHEN ("lochierarchy" = 0) THEN "i_category" END) ASC, "rank_within_parent" ASC
+LIMIT 100
+;
