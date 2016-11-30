cf2a209f5ce4f3a7f6bc83ea1012ba11bffcf2e3
Rebecca Schlussel <rebecca.schlussel@teradata.com>
2016-11-30 17:09:12 -0500

    Fix describe with aggregations and parameters
    Fixes aggregation analysis for describe input and output queries with
    parameters in the output expressions.

--- presto-main/src/main/java/com/facebook/presto/sql/analyzer/StatementAnalyzer.java
+++ presto-main/src/main/java/com/facebook/presto/sql/analyzer/StatementAnalyzer.java
@@ -1510,7 +1510,7 @@ class StatementAnalyzer
             Expression expression,
             Set<Expression> columnReferences)
     {
-        AggregationAnalyzer analyzer = new AggregationAnalyzer(groupByExpressions, metadata, scope, columnReferences, analysis.getParameters());
+        AggregationAnalyzer analyzer = new AggregationAnalyzer(groupByExpressions, metadata, scope, columnReferences, analysis.getParameters(), analysis.isDescribe());
         analyzer.analyze(expression);
     }
 
