71cb641c6d51c3d8ab562f088e47d4190868c6e6
Grzegorz Kokosiński <Grzegorz.Kokosinski@Teradata.com>
2016-11-30 08:21:47 +0100

    Fix q65 tpcds query

--- presto-product-tests/src/main/resources/sql-tests/testcases/tpcds/q65.sql
+++ presto-product-tests/src/main/resources/sql-tests/testcases/tpcds/q65.sql
@@ -1,4 +1,4 @@
--- database: presto_tpcds; groups: todo, tpcds; requires: com.teradata.tempto.fulfillment.table.hive.tpcds.ImmutableTpcdsTablesRequirements
+-- database: presto_tpcds; groups: tpcds; requires: com.teradata.tempto.fulfillment.table.hive.tpcds.ImmutableTpcdsTablesRequirements
 SELECT
   "s_store_name"
 , "i_item_desc"
