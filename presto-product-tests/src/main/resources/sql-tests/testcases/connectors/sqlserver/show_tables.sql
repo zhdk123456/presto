4002bb1178d4f84b997dc0541ce085bd6c8186c8
Sanjay Sharma <sanjay.sharma@teradata.com>
2016-12-14 20:04:14 -0500

    Add Sql-Server convention based product-tests

---
+++ presto-product-tests/src/main/resources/sql-tests/testcases/connectors/sqlserver/show_tables.sql
@@ -0,0 +1,13 @@
+-- database: presto; groups: sqlserver_connector; tables: sqlserver.dbo.workers_sqlserver
+-- queryType: SELECT;
+--!
+show tables from sqlserver.dbo
+--!
+-- delimiter: |; trimValues: true; ignoreOrder: true;
+msreplication_options|
+spt_fallback_db|
+spt_fallback_dev|
+spt_fallback_usg|
+spt_monitor|
+spt_values|
+workers_sqlserver|
