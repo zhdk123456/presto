49792d5ed163fb25f2036c7232ccf9cfecf10918
Piotr Findeisen <piotr.findeisen@teradata.com>
2017-01-11 13:22:15 +0100

    Extract TaskManagementExecutor from SqlTaskManager
    Before this change, task management executor was a private property of
    SqlTaskManager. To handle immediate memory revocation on MemoryPool
    over-allocation, the executor needs to be exposed to
    MemoryRevokingScheduler.

--- presto-main/src/main/java/com/facebook/presto/server/ServerMainModule.java
+++ presto-main/src/main/java/com/facebook/presto/server/ServerMainModule.java
@@ -36,6 +36,7 @@ import com.facebook.presto.execution.QueryPerformanceFetcherProvider;
 import com.facebook.presto.execution.SqlTaskManager;
 import com.facebook.presto.execution.TaskExecutor;
 import com.facebook.presto.execution.TaskInfo;
+import com.facebook.presto.execution.TaskManagementExecutor;
 import com.facebook.presto.execution.TaskManager;
 import com.facebook.presto.execution.TaskManagerConfig;
 import com.facebook.presto.execution.TaskStatus;
@@ -119,6 +120,7 @@ import com.facebook.presto.type.TypeRegistry;
 import com.facebook.presto.util.FinalizerService;
 import com.google.common.collect.ImmutableSet;
 import com.google.inject.Binder;
+import com.google.inject.Key;
 import com.google.inject.Provides;
 import com.google.inject.Scopes;
 import com.google.inject.TypeLiteral;
@@ -261,7 +263,9 @@ public class ServerMainModule
         // task execution
         jaxrsBinder(binder).bind(TaskResource.class);
         newExporter(binder).export(TaskResource.class).withGeneratedName();
-        binder.bind(TaskManager.class).to(SqlTaskManager.class).in(Scopes.SINGLETON);
+        binder.bind(TaskManagementExecutor.class).in(Scopes.SINGLETON);
+        binder.bind(SqlTaskManager.class).in(Scopes.SINGLETON);
+        binder.bind(TaskManager.class).to(Key.get(SqlTaskManager.class));
 
         // memory revoking scheduler
         binder.bind(MemoryRevokingScheduler.class).in(Scopes.SINGLETON);
