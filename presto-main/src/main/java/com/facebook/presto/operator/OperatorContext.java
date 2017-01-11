234cebd3d1d6ad2940567d3010a757bc1f4a2867
Piotr Findeisen <piotr.findeisen@teradata.com>
2017-01-11 13:22:14 +0100

    Fix synchronization of OperatorContext revocable memory state
    OperatorContext's fields pertaining revocable memory are accessed
    concurrently (from executing Operator and from MemoryRevokingScheduler),
    so they need to be synchronized.

--- presto-main/src/main/java/com/facebook/presto/operator/OperatorContext.java
+++ presto-main/src/main/java/com/facebook/presto/operator/OperatorContext.java
@@ -26,6 +26,8 @@ import com.google.common.util.concurrent.SettableFuture;
 import io.airlift.stats.CounterStat;
 import io.airlift.units.Duration;
 
+import javax.annotation.concurrent.GuardedBy;
+
 import java.lang.management.ManagementFactory;
 import java.lang.management.ThreadMXBean;
 import java.util.Optional;
@@ -43,7 +45,7 @@ import static java.util.Objects.requireNonNull;
 import static java.util.concurrent.TimeUnit.NANOSECONDS;
 
 /**
- * Only calling getOperatorStats is ThreadSafe
+ * Only {@link #getOperatorStats()} and revocable-memory-related operations are ThreadSafe
  */
 public class OperatorContext
 {
@@ -84,7 +86,18 @@ public class OperatorContext
     private final AtomicLong finishUserNanos = new AtomicLong();
 
     private final AtomicLong memoryReservation = new AtomicLong();
-    private final AtomicLong revocableMemoryReservation = new AtomicLong();
+    /*
+     * For reviewer: the revocable memory is a state accessed by multiple threads (the thread executing
+     * operator and memory revoking thread) and it requires synchronization. Since this class is currently
+     * not thread safe, the following options were considered:
+     * - apply & document synchronization of members related to revocable memory (currently implemented)
+     * - make this class thread safe (atomics are not enough, as e.g. `memoryFuture` and `memoryReservation`
+     *   should not be changed independently)
+     * - extract memory-related members to a new class (e.g. `MemoryReservation`) and make that class
+     *   thread safe
+     */
+    @GuardedBy("this")
+    private long revocableMemoryReservation = 0;
     private final OperatorSystemMemoryContext systemMemoryContext;
     private final OperatorSpillContext spillContext;
 
@@ -92,7 +105,8 @@ public class OperatorContext
     private final boolean collectTimings;
 
     // memoryRevokingRequestedFuture is done iff memory revoking was requested for operator
-    private final AtomicReference<SettableFuture<?>> memoryRevokingRequestedFuture = new AtomicReference<>();
+    @GuardedBy("this")
+    private SettableFuture<?> memoryRevokingRequestedFuture = SettableFuture.create();
 
     public OperatorContext(int operatorId, PlanNodeId planNodeId, String operatorType, DriverContext driverContext, Executor executor)
     {
@@ -109,8 +123,6 @@ public class OperatorContext
         this.memoryFuture.get().set(null);
         this.revocableMemoryFuture = new AtomicReference<>(SettableFuture.create());
         this.revocableMemoryFuture.get().set(null);
-
-        this.memoryRevokingRequestedFuture.set(SettableFuture.create());
 
         collectTimings = driverContext.isVerboseStats() && driverContext.isCpuTimerEnabled();
     }
@@ -235,15 +247,15 @@ public class OperatorContext
         memoryReservation.addAndGet(bytes);
     }
 
-    public void reserveRevocableMemory(long bytes)
+    public synchronized void reserveRevocableMemory(long bytes)
     {
         updateMemoryFuture(driverContext.reserveRevocableMemory(bytes), revocableMemoryFuture);
-        revocableMemoryReservation.addAndGet(bytes);
+        revocableMemoryReservation += bytes;
     }
 
-    public long getReservedRevocableBytes()
+    public synchronized long getReservedRevocableBytes()
     {
-        return revocableMemoryReservation.get();
+        return revocableMemoryReservation;
     }
 
     private static void updateMemoryFuture(ListenableFuture<?> memoryPoolFuture, AtomicReference<SettableFuture<?>> targetFutureReference)
@@ -280,11 +292,11 @@ public class OperatorContext
         }
     }
 
-    public void setRevocableMemoryReservation(long newRevocableMemoryReservation)
+    public synchronized void setRevocableMemoryReservation(long newRevocableMemoryReservation)
     {
         checkArgument(newRevocableMemoryReservation >= 0, "newRevocableMemoryReservation is negative");
 
-        long delta = newRevocableMemoryReservation - revocableMemoryReservation.get();
+        long delta = newRevocableMemoryReservation - revocableMemoryReservation;
 
         if (delta > 0) {
             reserveRevocableMemory(delta);
@@ -294,12 +306,12 @@ public class OperatorContext
         }
     }
 
-    public void freeRevocableMemory(long bytes)
+    public synchronized void freeRevocableMemory(long bytes)
     {
         checkArgument(bytes >= 0, "bytes is negative");
-        checkArgument(bytes <= revocableMemoryReservation.get(), "tried to free more revocable memory than is reserved");
+        checkArgument(bytes <= revocableMemoryReservation, "tried to free more revocable memory than is reserved");
         driverContext.freeRevocableMemory(bytes);
-        revocableMemoryReservation.getAndAdd(-bytes);
+        revocableMemoryReservation -= bytes;
     }
 
     public void freeMemory(long bytes)
@@ -383,30 +395,36 @@ public class OperatorContext
         return true;
     }
 
-    public boolean isMemoryRevokingRequested()
+    public synchronized boolean isMemoryRevokingRequested()
     {
-        return memoryRevokingRequestedFuture.get().isDone();
+        return memoryRevokingRequestedFuture.isDone();
     }
 
-    public void requestMemoryRevoking()
+    /**
+     * Returns amount of memory which revoking was requested.
+     */
+    public synchronized long requestMemoryRevoking()
     {
-        memoryRevokingRequestedFuture.get().set(null);
+        boolean alreadyRequested = isMemoryRevokingRequested();
+        if (!alreadyRequested && revocableMemoryReservation > 0) {
+            memoryRevokingRequestedFuture.set(null);
+            return revocableMemoryReservation;
+        }
+        return 0;
     }
 
-    public void resetMemoryRevokingRequested()
+    public synchronized void resetMemoryRevokingRequested()
     {
-        SettableFuture<?> currentFuture = memoryRevokingRequestedFuture.get();
+        SettableFuture<?> currentFuture = memoryRevokingRequestedFuture;
         if (!currentFuture.isDone()) {
             return;
         }
-        memoryRevokingRequestedFuture.compareAndSet(currentFuture, SettableFuture.create());
-        // if we do not change the value of currentFuture we are still good as this means other thread
-        // changed it to SettableFuture.create in exactly same method.
+        memoryRevokingRequestedFuture = SettableFuture.create();
     }
 
-    public SettableFuture<?> getMemoryRevokingRequestedFuture()
+    public synchronized SettableFuture<?> getMemoryRevokingRequestedFuture()
     {
-        return memoryRevokingRequestedFuture.get();
+        return memoryRevokingRequestedFuture;
     }
 
     public void setInfoSupplier(Supplier<OperatorInfo> infoSupplier)
@@ -473,7 +491,7 @@ public class OperatorContext
                 new Duration(finishUserNanos.get(), NANOSECONDS).convertToMostSuccinctTimeUnit(),
 
                 succinctBytes(memoryReservation.get()),
-                succinctBytes(revocableMemoryReservation.get()),
+                succinctBytes(getReservedRevocableBytes()),
                 succinctBytes(systemMemoryContext.getReservedBytes()),
                 memoryFuture.get().isDone() ? Optional.empty() : Optional.of(WAITING_FOR_MEMORY),
                 info);
