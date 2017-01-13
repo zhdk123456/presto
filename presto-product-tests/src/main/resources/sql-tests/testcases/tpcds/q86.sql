050f1b8a35603c44d791e8af8653b0f9b85ef0b2
Lukasz Osipiuk <lukasz@osipiuk.net>
2017-01-13 15:18:30 +0100

    Unquarantine TPC-DS q86

--- presto-product-tests/src/main/resources/sql-tests/testcases/tpcds/q86.sql
+++ presto-product-tests/src/main/resources/sql-tests/testcases/tpcds/q86.sql
@@ -1,5 +1,4 @@
--- database: presto_tpcds; groups: tpcds, quarantine; requires: com.teradata.tempto.fulfillment.table.hive.tpcds.ImmutableTpcdsTablesRequirements
---- quarantined as grouping is not yet supported, see https://github.com/prestodb/presto/pull/6704
+-- database: presto_tpcds; groups: tpcds; requires: com.teradata.tempto.fulfillment.table.hive.tpcds.ImmutableTpcdsTablesRequirements
 SELECT
   "sum"("ws_net_paid") "total_sum"
 , "i_category"
