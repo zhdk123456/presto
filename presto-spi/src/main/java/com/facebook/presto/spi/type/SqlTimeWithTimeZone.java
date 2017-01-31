da4f4be9a4569fb4f40cd6526e2df79e89130aa6
Andrzej Fiedukowicz <Andrzej.Fiedukowicz@teradata.com>
2017-01-31 16:16:23 +0100

    Fix TZ issue in TIME WITH TZ for TZ ID changing offset over time
    Some Timezone definitions are not consistent over time.
    For example: Asia/Katmandu had different offset (+5:30) in 1970
    than currently (+5:45).
    For that reason using millisUtc representing only time that passed
    from midnight UTC to the represented time is invalid as Instance::atZone
    will always use TZ offset that was valid on `1970-01-01`.
    
    To avoid breaking SPI compatibility by changing millisUtc semantic
    current TZ offset is obtained from System.currentTimeMillis().
    It's possibly not perfect, but having information SPI provide there
    is probably no better way of doing that.
    
    Additionally some checks were added to avoid possible misunderstanding
    of millisUtc semantic (by providing full timestamp).

--- presto-spi/src/main/java/com/facebook/presto/spi/type/SqlTimeWithTimeZone.java
+++ presto-spi/src/main/java/com/facebook/presto/spi/type/SqlTimeWithTimeZone.java
@@ -16,7 +16,9 @@ package com.facebook.presto.spi.type;
 import com.fasterxml.jackson.annotation.JsonValue;
 
 import java.time.Instant;
+import java.time.LocalTime;
 import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Objects;
 import java.util.TimeZone;
@@ -24,22 +26,54 @@ import java.util.TimeZone;
 import static com.facebook.presto.spi.type.DateTimeEncoding.unpackMillisUtc;
 import static com.facebook.presto.spi.type.DateTimeEncoding.unpackZoneKey;
 
+/**
+ * This type represent instant of time in some day and timezone, where timezone
+ * is known and day is not.
+ */
 public final class SqlTimeWithTimeZone
 {
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss.SSS VV");
 
+    /**
+     * As we dont have negative timestamps and max value is midnight in timezone that is ~(UTC -12:00),
+     * just not to care about precision we do use ~(UTC -24:00).
+     *
+     * It's all about protecting type from beeing used with full millisUTC.
+     */
+    private static final long MAX_MILLIS_TIME = 2 * 24 * 60 * 60 * 1000;
+
+    /**
+     * Milliseconds relative to midnight UTC of the given time (millisUtc < MAX_MILLIS_TIME).
+     */
     private final long millisUtc;
     private final TimeZoneKey timeZoneKey;
+
+    /**
+     * FIXME This is used only to determine last of TZ shifts for TIME
+     * This is workaround to avoid chaning semantic of millisUTC being part of SPI
+     * This is best information about current TZ we can get at the point of query
+     *
+     * eg. Asia/Katmandu was +5:30 at some point in a history and then was changed
+     *     to +5:45, without usage of this timestamp, we would use TZ shift valid
+     *     on 1970-01-01, which is invalid today.
+     */
+    private final long timezoneOffsetReferenceTimestampUtc = System.currentTimeMillis();
 
     public SqlTimeWithTimeZone(long timeWithTimeZone)
     {
         millisUtc = unpackMillisUtc(timeWithTimeZone);
+        if (millisUtc > MAX_MILLIS_TIME) {
+            throw new RuntimeException("millisUtc must be time in millisecond that past since midnight UTC.");
+        }
         timeZoneKey = unpackZoneKey(timeWithTimeZone);
     }
 
     public SqlTimeWithTimeZone(long millisUtc, TimeZoneKey timeZoneKey)
     {
         this.millisUtc = millisUtc;
+        if (millisUtc > MAX_MILLIS_TIME) {
+            throw new RuntimeException("millisUtc must be time in millisecond that past since midnight UTC.");
+        }
         this.timeZoneKey = timeZoneKey;
     }
 
@@ -83,6 +117,18 @@ public final class SqlTimeWithTimeZone
     @Override
     public String toString()
     {
-        return Instant.ofEpochMilli(millisUtc).atZone(ZoneId.of(timeZoneKey.getId())).format(formatter);
+        // FIXME This is hack that we need to use as the timezone interpretation depends on date (not only on time)
+        // Additionally we cannot just grab current TZ offset from timezoneOffsetReferenceTimestampUtc and use it
+        // as our formatting library will fail to display expected TZ symbol.
+        long currentMillisOfDay =
+                LocalTime.from(
+                        Instant.ofEpochMilli(timezoneOffsetReferenceTimestampUtc)
+                               .atZone(ZoneId.of(TimeZoneKey.UTC_KEY.getId())))
+                        .toNanoOfDay() / 1_000_000L; // nanos to millis
+        long timeMillisUtcInCurrentDay = timezoneOffsetReferenceTimestampUtc - currentMillisOfDay + millisUtc;
+
+        Instant utcInstantInCurrentDay = Instant.ofEpochMilli(timeMillisUtcInCurrentDay);
+        ZonedDateTime zonedDateTime = utcInstantInCurrentDay.atZone(ZoneId.of(timeZoneKey.getId()));
+        return zonedDateTime.format(formatter);
     }
 }
