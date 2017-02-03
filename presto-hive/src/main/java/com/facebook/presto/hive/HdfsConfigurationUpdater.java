ac8c90ef5d2ae57d676944058c1461ce0f7fbee8
Andrii Rosa <Andriy.Rosa@TERADATA.COM>
2017-02-03 15:16:54 -0500

    Fix hive.config.resources property behaviour
    `hive.config.resources` property allows to set additional resources
    that must override the default properties for HDFS.
    
    `Configuration#addResource` method loads the resource, and handles the
    properties from that resource kind of like a defaults. So if before
    some property has been set explicitly via `set*` method, that property
    won't be overwritten with the value loaded from the resource.
    
    The way how defaults are being populated to the configuration uses
    `set` method. So, further properties loaded from the specified resources
    can't override the defaults.
    
    This commits loads the resource into temporary clean configuration. And
    then copies all the properties to the destination using the `set` method,
    so that way the defaults are being actually overwritten.

--- presto-hive/src/main/java/com/facebook/presto/hive/HdfsConfigurationUpdater.java
+++ presto-hive/src/main/java/com/facebook/presto/hive/HdfsConfigurationUpdater.java
@@ -31,6 +31,7 @@ import javax.net.SocketFactory;
 
 import java.io.File;
 import java.util.List;
+import java.util.Map;
 
 import static com.facebook.hive.orc.OrcConf.ConfVars.HIVE_ORC_COMPRESSION;
 import static com.google.common.base.Preconditions.checkArgument;
@@ -112,7 +113,7 @@ public class HdfsConfigurationUpdater
     {
         if (resourcePaths != null) {
             for (String resourcePath : resourcePaths) {
-                config.addResource(new Path(resourcePath));
+                addResource(config, new Path(resourcePath));
             }
         }
 
@@ -227,6 +228,15 @@ public class HdfsConfigurationUpdater
         public void reloadCachedMappings()
         {
             // no-op
+        }
+    }
+
+    private static void addResource(Configuration config, Path resource)
+    {
+        Configuration resourceProperties = new Configuration(false);
+        resourceProperties.addResource(resource);
+        for (Map.Entry<String, String> entry : resourceProperties) {
+            config.set(entry.getKey(), entry.getValue());
         }
     }
 }
