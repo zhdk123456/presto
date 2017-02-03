e773044aff9b8855f815e06709eb8451fb7dd9d7
Artur Gajowy <artur.gajowy@gmail.com>
2017-02-03 02:58:10 +0100

    fixup! Refactor PushLimitThroughMarkDistinct to leverage Optional's methods

--- presto-main/src/main/java/com/facebook/presto/sql/planner/iterative/rule/PushLimitThroughMarkDistinct.java
+++ presto-main/src/main/java/com/facebook/presto/sql/planner/iterative/rule/PushLimitThroughMarkDistinct.java
@@ -24,7 +24,7 @@ import com.facebook.presto.sql.planner.plan.PlanNode;
 import java.util.Optional;
 
 import static com.facebook.presto.sql.planner.iterative.rule.Util.transpose;
-import static com.facebook.presto.util.Optionals.cast;
+import static com.facebook.presto.util.Optionals.tryCast;
 
 public class PushLimitThroughMarkDistinct
         implements Rule
@@ -32,7 +32,7 @@ public class PushLimitThroughMarkDistinct
     @Override
     public Optional<PlanNode> apply(PlanNode node, Lookup lookup, PlanNodeIdAllocator idAllocator, SymbolAllocator symbolAllocator)
     {
-        return cast(node, LimitNode.class)
+        return tryCast(node, LimitNode.class)
                 .flatMap(parent -> lookup.resolve(parent.getSource(), MarkDistinctNode.class)
                 .map(child -> transpose(parent, child)));
     }
