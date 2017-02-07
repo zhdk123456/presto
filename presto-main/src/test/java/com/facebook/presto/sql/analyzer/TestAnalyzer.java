d21af4c2ba773a659d01555165ff4597897e5dd3
Karol Sobczak <Karol.Sobczak@Teradata.com>
2017-02-07 01:36:29 +0100

    Proper analysis and planning of complex ORDER BY expressions
    Previously analyser was rewriting ORDER BY expressions which
    were using QuerySpecification output fields. This wasn't
    compliant with how the analyer/planner works (analyzer shouldn't
    modify AST) and caused issues with coercions.
    This commit adds proper analysis and planning of complex ORDER BY
    expressions. An intermediate PlanBuilder for ORDER BY scope
    is created during planning which correctly resolves column
    expressions. Additionally, AST tree stays unmodified.
    
    This fixes: https://github.com/prestodb/presto/issues/7142

--- presto-main/src/test/java/com/facebook/presto/sql/analyzer/TestAnalyzer.java
+++ presto-main/src/test/java/com/facebook/presto/sql/analyzer/TestAnalyzer.java
@@ -90,6 +90,7 @@ import static com.facebook.presto.sql.analyzer.SemanticErrorCode.NESTED_WINDOW;
 import static com.facebook.presto.sql.analyzer.SemanticErrorCode.NON_NUMERIC_SAMPLE_PERCENTAGE;
 import static com.facebook.presto.sql.analyzer.SemanticErrorCode.NOT_SUPPORTED;
 import static com.facebook.presto.sql.analyzer.SemanticErrorCode.ORDER_BY_MUST_BE_IN_SELECT;
+import static com.facebook.presto.sql.analyzer.SemanticErrorCode.REFERENCE_TO_OUTPUT_ATTRIBUTE_WITHIN_ORDER_BY_AGGREGATION;
 import static com.facebook.presto.sql.analyzer.SemanticErrorCode.SAMPLE_PERCENTAGE_OUT_OF_RANGE;
 import static com.facebook.presto.sql.analyzer.SemanticErrorCode.SCHEMA_NOT_SPECIFIED;
 import static com.facebook.presto.sql.analyzer.SemanticErrorCode.STANDALONE_LAMBDA;
@@ -184,6 +185,15 @@ public class TestAnalyzer
         analyze("SELECT * FROM t1 WHERE (VALUES 1) IN (VALUES 1)");
         analyze("SELECT * FROM t1 WHERE (VALUES 1) IN (2)");
         analyze("SELECT * FROM (SELECT 1) t1(x) WHERE x IN (SELECT 1)");
+    }
+
+    @Test
+    public void testReferenceToOutputColumnFromOrderByAggregation()
+            throws Exception
+    {
+        assertFails(REFERENCE_TO_OUTPUT_ATTRIBUTE_WITHIN_ORDER_BY_AGGREGATION, "SELECT max(a) AS a FROM (values (1,2)) t(a,b) GROUP BY b ORDER BY max(a+b)");
+        assertFails(REFERENCE_TO_OUTPUT_ATTRIBUTE_WITHIN_ORDER_BY_AGGREGATION, "SELECT DISTINCT a AS a, max(a) AS c from (VALUES (1, 2)) t(a, b) GROUP BY a ORDER BY max(a)");
+        assertFails(REFERENCE_TO_OUTPUT_ATTRIBUTE_WITHIN_ORDER_BY_AGGREGATION, "SELECT CAST(ROW(1) AS ROW(someField BIGINT)) AS a FROM (values (1,2)) t(a,b) GROUP BY b ORDER BY MAX(a.someField)");
     }
 
     @Test
@@ -343,6 +353,7 @@ public class TestAnalyzer
     {
         // TODO: analyze output
         analyze("SELECT a x FROM t1 ORDER BY x + 1");
+        analyze("SELECT max(a) FROM (values (1,2), (2,1)) t(a,b) GROUP BY b ORDER BY max(b*1.0)");
     }
 
     @Test
