b9eede0947aea962cb4ed436b55ef4c849e686de
Andrzej Fiedukowicz <Andrzej.Fiedukowicz@teradata.com>
2017-02-15 15:58:22 +0100

    Revert "Change default TIME/TIMESTAMP semantic to ANSI SQL"
    This reverts commit 037e77572d2e1ef3820995a85886add509ace93c.

--- presto-main/src/test/java/com/facebook/presto/operator/scalar/TestDateTimeFunctions.java
+++ presto-main/src/test/java/com/facebook/presto/operator/scalar/TestDateTimeFunctions.java
@@ -144,7 +144,7 @@ public class TestDateTimeFunctions
     private static void assertCurrentDateAtInstant(TimeZoneKey timeZoneKey, long instant)
     {
         long expectedDays = epochDaysInZone(timeZoneKey, instant);
-        long dateTimeCalculation = currentDate(new TestingConnectorSession("test", timeZoneKey, US, instant, ImmutableList.of(), ImmutableMap.of(), false));
+        long dateTimeCalculation = currentDate(new TestingConnectorSession("test", timeZoneKey, US, instant, ImmutableList.of(), ImmutableMap.of(), true));
         assertEquals(dateTimeCalculation, expectedDays);
     }
 
