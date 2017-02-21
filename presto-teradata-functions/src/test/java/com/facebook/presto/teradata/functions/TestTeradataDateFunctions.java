1e9c5b3b8d1e2443bbc6f866cd1ce6ab2efb81ee
Andrzej Fiedukowicz <Andrzej.Fiedukowicz@teradata.com>
2017-02-21 17:12:39 +0100

    fixup! Avoid using deprecated SqlTime(stamp) constructor based on session

--- presto-teradata-functions/src/test/java/com/facebook/presto/teradata/functions/TestTeradataDateFunctions.java
+++ presto-teradata-functions/src/test/java/com/facebook/presto/teradata/functions/TestTeradataDateFunctions.java
@@ -29,8 +29,8 @@ import static com.facebook.presto.metadata.FunctionExtractor.extractFunctions;
 import static com.facebook.presto.spi.type.TimeZoneKey.getTimeZoneKey;
 import static com.facebook.presto.spi.type.VarcharType.VARCHAR;
 import static com.facebook.presto.testing.TestingSession.testSessionBuilder;
-import static com.facebook.presto.type.TimestampOperators.castToDate;
 import static com.facebook.presto.testing.TestingSqlTime.sqlTimestampOf;
+import static com.facebook.presto.type.TimestampOperators.castToDate;
 import static com.facebook.presto.util.DateTimeZoneIndex.getDateTimeZone;
 import static java.lang.Math.toIntExact;
 
