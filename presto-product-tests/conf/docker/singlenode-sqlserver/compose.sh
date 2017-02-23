8c7d51806e59f322cfd5eef6a4ae4332af6400e4
Sanjay Sharma <sanjay.sharma@teradata.com>
2017-02-23 11:21:20 -0500

    fixup! Add travis job for SQL Server tests

--- presto-product-tests/conf/docker/singlenode-sqlserver/compose.sh
+++ presto-product-tests/conf/docker/singlenode-sqlserver/compose.sh
@@ -2,9 +2,12 @@
 
 source ${BASH_SOURCE%/*}/../common/compose-commons.sh
 
+TEMPTO_CONFIG_YAML_DEFAULT="${PRODUCT_TESTS_ROOT}/conf/tempto/tempto-configuration-for-docker-default.yaml"
+TEMPTO_CONFIG_YAML=${TEMPTO_CONFIG_YAML:-${TEMPTO_CONFIG_YAML_DEFAULT}}
+export_canonical_path TEMPTO_CONFIG_YAML
+
 docker-compose \
 -f ${BASH_SOURCE%/*}/../common/standard.yml \
--f ${BASH_SOURCE%/*}/../common/jdbc_db.yml \
 -f ${BASH_SOURCE%/*}/../common/cassandra.yml \
 -f ${BASH_SOURCE%/*}/docker-compose.yml \
 "$@"
